f1c2297d3108b7e66b4fe132041bc32c1f3178c9
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-06 16:59:07 +0530

    Time: 3 ms (98.44%) | Memory: 59.3 MB (19.06%) - LeetSync

--- 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
@@ -1,43 +1,30 @@
 class Solution {
     public int getMaxLen(int[] nums) {
-        int left =0,right=0,negativeCount=0;
-        boolean isPositive = true;
-        int maxi = 0,len = nums.length;
-
-        for(int i=0;i<len;i++)
+        int maxi =0, positiveLength =0, negativeLength=0;
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]<0)negativeCount++;
-        }
-
-        while(right<len)
-        {
-            if(nums[right]<0)
+            if(nums[i]==0)
             {
-                negativeCount--;
-                isPositive = !isPositive;
-
-                if(!isPositive && negativeCount == 0) {
-                   while(nums[left]>0) left++;
-                   left++;
-                   isPositive = true;
-                }
+                positiveLength =0;
+                negativeLength =0;
             }
-            else if(nums[right]==0)
+            else if(nums[i]>0)
             {
-                if(!isPositive)
-                 {
-                   while(nums[left]>0) left++;
-                   left++;
-                   maxi = Math.max(maxi,right-left);
-                 }
-                left = right+1;
-                right++;
-                isPositive = true;
-                continue;
+                positiveLength++;
+                negativeLength = (negativeLength ==0)? 0: negativeLength +1;
             }
+            else
+            {
+                //swap
+                int temp = negativeLength;
+                negativeLength = positiveLength;
+                positiveLength = temp;
 
-            if(isPositive) maxi = Math.max(maxi,right-left+1);
-            right++;
+                negativeLength++;
+                if(temp!=0) positiveLength++;
+            }
+            
+            maxi = Math.max(maxi,positiveLength);
         }
 
         return maxi;
