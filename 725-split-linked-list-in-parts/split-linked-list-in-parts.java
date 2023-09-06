ce7c93b818125497752a6787a2d8424db68a9c60
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-06 06:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (38.88%) - LeetSync

---
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    int getLength(ListNode head)
+    {
+        ListNode temp = head;
+        int len =0;
+        while(temp!=null)
+        {
+            len++;
+            temp = temp.next;
+        }
+        return len;
+    }
+
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int len = getLength(head);
+        int sizeNormal  = len/k;
+        int nodeNormal =  k- len%k;
+        int sizeExtra  =  sizeNormal+1;
+        int nodeExtra = len%k;
+
+        ListNode temp = head;
+        ListNode[] ans = new ListNode[k];
+        int pointer =0;
+        while(nodeExtra>0)
+        {
+            int loopTill = sizeExtra;
+            ListNode dummy = temp;
+            ans[pointer] = dummy;
+
+            while(loopTill>1)
+            {
+                temp = temp.next;
+                loopTill--;
+            }
+            ListNode prev = temp;
+            temp = temp.next;
+            prev.next = null;  
+
+            nodeExtra--;  
+            pointer++;    
+        }
+
+        while(nodeNormal>0)
+        {
+            int loopTill = sizeNormal;
+            ListNode dummy = temp;
+            ans[pointer] = dummy;
+
+            while(loopTill>1)
+            {
+                temp = temp.next;
+                loopTill--;
+            }
+            
+            
+            if(temp!=null)
+            {
+            ListNode prev = temp;
+            temp = temp.next;
+            prev.next = null; 
+            }
+
+            nodeNormal--;
+            pointer++;
+        }
+
+        return ans;
+
+    }
+}
