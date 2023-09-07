b953a029c9eb9f889e98aacf151e2918f86f02c3
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-07 21:00:14 +0530

    Time: 200 ms (38.81%) | Memory: 44 MB (13.64%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,46 @@
+class Solution {
+    boolean dfs(int vis[][],char[][] board, int row, int col, int m, int n,String word,int index)
+    {
+        if(index==word.length()-1) 
+        {
+         if(word.charAt(index) != board[row][col]) return false;
+         else return true;
+        }
+
+        if(word.charAt(index) != board[row][col]) return false;
+        vis[row][col]=1;
+        int delRow[] = {-1,1,0,0};
+        int delCol[] = {0,0,1,-1};
+        for(int i=0;i<4;i++)
+        {
+            int newRow = row+delRow[i];
+            int newCol = col+delCol[i];
+            if(newRow>=0 && newCol>=0 && newCol<n && newRow<m && vis[newRow][newCol]==0)
+            {
+               boolean res =  dfs(vis, board, newRow, newCol,m,n,word,index+1);
+               if(res) return res;
+            }
+        }
+
+        vis[row][col] = 0;
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        int vis[][] = new int[m][n];
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(word.charAt(0)==board[i][j])
+                {
+                    boolean res = dfs(vis,board,i,j,m,n,word,0);
+                    if(res) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
