f59bf3264a3ac1cbd798f52b79b200cae44b8f88
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-10 11:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (23.93%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,21 +1,24 @@
 class Solution {
 
-    int rec(int inOrder[] , int left ,int right)
+    int rec(int inOrder[] , int left ,int right,int dp[][] )
     {
         if(right<=left) return 1;
-       
+       if(dp[left][right]!=-1) return dp[left][right];
 
         int total=0;
         for(int i=left;i<=right;i++)
         {
-            total+= rec(inOrder,left,i-1)*rec(inOrder,i+1,right);
+            total+= rec(inOrder,left,i-1,dp)*rec(inOrder,i+1,right,dp);
         }
-        return total;
+        return dp[left][right] = total;
     }
     public int numTrees(int n) {
         int inorder[] = new int[n];
+        int dp[][] = new int[n][n];
+        for(int row[] : dp) Arrays.fill(row,-1);
+
         for(int i=0;i<n;i++)inorder[i]=i+1;
-        return rec(inorder,0,n-1);
+        return rec(inorder,0,n-1,dp);
 
     }
 }
