eaac024ed81f6c860f127111d8ea124c0e045e61
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-10 16:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (53.66%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -2,14 +2,13 @@ class Solution {
     double fact(int n)
     {
         if(n==1|| n==0) return 1;
-        
-        return ((n)*(n-1)*(fact(n-2)%(double)(1e9+7))/2.0)%(double)(1e9+7);
+        return ((n)*(n-1)*(fact(n-2))/2.0)%(double)(1e9+7);
     }
 
     public int countOrders(int n) {
        int noOfSpaces = 2*n;
-       long a = (long)fact(noOfSpaces)%(long)(1e9+7);
-       return (int)(a);
+      
+       return (int)fact(noOfSpaces);
 
     }
 }
