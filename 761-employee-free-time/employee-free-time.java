845741e275447928cd381e47bbb482f93f0761fa
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-09-28 09:48:10 +0530

    Time: 11 ms (64.25%) | Memory: 44.5 MB (96.85%) - LeetSync

--- 761-employee-free-time/employee-free-time.java
+++ 761-employee-free-time/employee-free-time.java
@@ -12,65 +12,25 @@ class Interval {
     }
 };
 */
-class CustomComparator implements Comparator<Interval>
-{
-    public int compare(Interval a, Interval b)
-    {
-        if(a.start==b.start) return a.end - b.end;
-        return a.start- b.start;
-    }
-}
-class Solution {
 
-    void buildIntervals(List<List<Interval>> schedule,List<Interval> intervals)
-    {
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        PriorityQueue<Interval>  pq = new PriorityQueue<>((a,b)->(a.start-b.start));
+        List<Interval> freeTime = new ArrayList<>();
         for(int i=0;i<schedule.size();i++)
         {
             for(int j=0;j<schedule.get(i).size();j++)
             {
-                intervals.add(schedule.get(i).get(j));
+                pq.add(schedule.get(i).get(j));
             }
         }
-    }
 
-    void mergeIntervals(List<Interval> intervals,List<Interval> mergedIntervals)
-    {
-        for(int  i = 0;i<intervals.size();i++)
+        Interval prev = pq.poll();
+        while(!pq.isEmpty())
         {
-            int curStart = intervals.get(i).start;
-            int curEnd = intervals.get(i).end;
-            int prevStart =(!mergedIntervals.isEmpty())?
-            mergedIntervals.get(mergedIntervals.size()-1).start:0;
-            int prevEnd = (!mergedIntervals.isEmpty())?
-            mergedIntervals.get(mergedIntervals.size()-1).end:-1;
-
-            if(mergedIntervals.isEmpty() ||curStart>prevEnd)
-            {
-                mergedIntervals.add(new Interval(curStart,curEnd));
-            }
-            else
-            {
-            mergedIntervals.set(mergedIntervals.size()-1,new Interval(prevStart,Math.max(prevEnd,curEnd)));
-            }
-        }
-    }
-
-    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> intervals = new ArrayList<>();
-        List<Interval> mergedIntervals = new ArrayList<>();
-        List<Interval> freeTime = new ArrayList<>();
-
-        //build Interval
-        buildIntervals(schedule,intervals);
-        Collections.sort(intervals,new CustomComparator());
-        //merge interval
-        mergeIntervals(intervals,mergedIntervals);
-        //get free time
-        for(int i=0;i<mergedIntervals.size()-1;i++)
-        {
-            int start = mergedIntervals.get(i).end;
-            int end = mergedIntervals.get(i+1).start;
-            freeTime.add(new Interval(start,end));
+            Interval next = pq.poll();
+            if(next.start>prev.end) {freeTime.add(new Interval(prev.end,next.start)); prev.end = next.end;}
+            else prev.end = Math.max(next.end,prev.end);
         }
         return freeTime;
     }
