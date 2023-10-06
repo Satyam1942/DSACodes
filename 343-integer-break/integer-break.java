6d36ab7a56f5e561b9ec720adbb54f014b62da40
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-10-06 06:42:41 +0530

    Time: 2 ms (17.67%) | Memory: 39.3 MB (39.77%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,29 +1,22 @@
 class Solution {
-    int calcMaximumProduct(int n ,int temp, int []dp)
-    {
 
-         if(temp<=0)
-         {
-             if(temp==0) return 1;
-             else return 0;
-         }
-
-        if(dp[temp]!=-1) return dp[temp];
-
-        int maxi = Integer.MIN_VALUE;
-        for(int i=1;i<n;i++)
-        {
-            int pro = i*calcMaximumProduct(n,temp-i,dp);
-            maxi = Math.max(maxi,pro);
-        }
-
-        return dp[temp] = maxi;
-
-    }
     public int integerBreak(int n) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
+        dp[0]=1;
 
-       return calcMaximumProduct(n,n,dp);
+        for(int temp = 1;temp<=n;temp++)
+        {
+            int maxi = Integer.MIN_VALUE;
+            for(int i=1;i<n;i++)
+            {
+                int pro = Integer.MIN_VALUE;
+                 if(temp>=i) 
+                    pro = i*dp[temp-i];
+                maxi = Math.max(maxi,pro);
+            }
+             dp[temp] = maxi;
+        }
+
+       return dp[n];
     }
 }
