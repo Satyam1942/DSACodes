4919ffaccf2b7b0903b3a810f7f09c6dec0c8a22
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-10-08 11:28:11 +0530

    Time: 184 ms (100.00%) | Memory: 60 MB (100.00%) - LeetSync

--- 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.java
+++ 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.java
@@ -9,29 +9,28 @@ class Solution {
         
         if(dp[i][count][flip]!=null) return  dp[i][count][flip];
 
+        int temp = (int)(1e8);
         if((s1.charAt(i)==s2.charAt(i) && flip==0) || (s1.charAt(i)!=s2.charAt(i) && flip==1))
         {
-            return dp[i][count][flip] = rec(i+1,count,0,s1,s2,n,x,dp);
+            temp = rec(i+1,count,0,s1,s2,n,x,dp);
         }
         else
         {
-            int case1 = 1+rec(i+1,count,1, s1,s2,n,x,dp);
+            int case1 = Integer.MAX_VALUE;
+            if(i!=n-1) case1 =  1+rec(i+1,count,1, s1,s2,n,x,dp);
             int case2 = rec(i+1,count+1,0,s1,s2,n,x,dp);
-            return dp[i][count][flip] =  Math.min(case1,case2);
+            temp =  Math.min(case1,case2);
         }
+
+         return dp[i][count][flip] = temp;
     }
     public int minOperations(String s1, String s2, int x) {
-        int noOfChanges = 0;
         int n = s1.length();
-        for(int i=0;i<n;i++)
-        {
-            if(s1.charAt(i)!=s2.charAt(i)) noOfChanges++;
-        }
-        
-        if(noOfChanges%2!=0) return -1;
 
         Integer dp[][][] = new Integer[n][n][2];
-       return  rec(0,0,0,s1,s2,n,x,dp);
+        int temp =   rec(0,0,0,s1,s2,n,x,dp);
+        if(temp==(int)(1e8)) return -1;
+        else return temp;
             
     }
 }
