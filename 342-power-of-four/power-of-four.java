ff2284daf10666ad47c031d256f607d7ad63896d
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-10-23 07:36:54 +0530

    Time: 1 ms (76.31%) | Memory: 39.1 MB (96.30%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        int mask = 1, countZ = 0, countO = 0;
-        boolean flag = false;
-
-        for(int i=0;i<32;i++)
-        {
-            if((n&(1<<i))==0) 
-            {
-                if(!flag)
-                countZ++;
-            }
-            else {countO++; flag = true;}
-            
-        }
-
-        return countZ%2==0 && countO==1;
+        return n>0 && (n&(n-1))==0 && (n& 0x55555555)!=0;
     }
 }
