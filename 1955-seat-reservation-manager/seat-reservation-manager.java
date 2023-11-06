ce2b7290b24f536caa4cb5b65dc8509d72025b5e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-11-06 06:28:09 +0530

    Time: 128 ms (6.35%) | Memory: 95 MB (6.35%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,21 +1,18 @@
 class SeatManager {
-    TreeSet<Integer> reserved;
+    
     TreeSet<Integer> unreserved;
     
     public SeatManager(int n) {
-        reserved = new TreeSet<>();
         unreserved = new TreeSet<>();
         for(int i=1;i<=n;i++)unreserved.add(i);
     }
     
     public int reserve() {
         int seatNo = unreserved.pollFirst();
-        reserved.add(seatNo);
         return seatNo;
     }
     
     public void unreserve(int seatNumber) {
-        reserved.remove(seatNumber);
         unreserved.add(seatNumber);
     }
 }
