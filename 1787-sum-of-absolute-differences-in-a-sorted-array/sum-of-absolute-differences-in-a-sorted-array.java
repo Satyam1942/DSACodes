74ca516694d001d1113a8a7061bf198db6ce46a7
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-11-25 08:45:45 +0530

    Time: 4 ms (65.38%) | Memory: 59.4 MB (27.56%) - LeetSync

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int differenceMaxVal  =  0;
+        int length = nums.length;
+        for(int i=0;i<length;i++)
+        {
+            differenceMaxVal+= Math.abs(nums[length-1]-nums[i]);
+        }
+
+        int ans[] = new int[length];
+        ans[length-1] =  differenceMaxVal;
+
+        for(int i=length-2;i>=0;i--)
+        {
+            int difNextNum = nums[i+1]-nums[i];
+            ans[i] = differenceMaxVal + ((length-i-1)-(i+1))*difNextNum; 
+            differenceMaxVal = ans[i];
+        }
+        
+        return ans;
+    }
+}
+
+/*
+5-  
+3,2,0 =5 
+3-
+2+
+5-(2*1) = 3
+5-(3*1) = 
+if you know a-b ;
+            a-c;
+            b-c by just subtracting
+            5-3
+            5-2
+            5= {3,2,0} = 5
+                3 = 2 subtract all by 2 
+                5 + (2*1) - (2*2) = 3
+
+                2 = 1 
+                5+ (no of index after 2*1)- (no of index  before 2)*1
+                3+ (2*1) - 1*1
+
+ */
