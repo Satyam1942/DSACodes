3d34a92b897421da6de6aece7a236db62965156f
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-11-27 20:48:28 +0530

    Time: 1 ms (99.61%) | Memory: 40.8 MB (59.14%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.java
+++ 956-number-of-music-playlists/number-of-music-playlists.java
@@ -8,19 +8,19 @@ class Solution {
             if(oldSongs==n) return  1; 
             else return 0;
         }
+        
         if(dp[goal][oldSongs]!=null) return dp[goal][oldSongs];
 
         long playOld =0l, playNew = 0l;
 
        if(oldSongs>k)  playOld = (oldSongs-k)*rec(n,goal-1,k,oldSongs,dp);
-
-       playNew = (n-oldSongs)*rec(n,goal-1,k,oldSongs+1,dp);
+       if(n>oldSongs) playNew = (n-oldSongs)*rec(n,goal-1,k,oldSongs+1,dp);
 
        return dp[goal][oldSongs] = (playOld+playNew)%mod;
     }
 
     public int numMusicPlaylists(int n, int goal, int k) {
-        Long dp[][] = new Long[goal+1][goal+n];
+        Long dp[][] = new Long[goal+1][n+1];
 
         return (int)rec(n,goal,k,0,dp);
     }
