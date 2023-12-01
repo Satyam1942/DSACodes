7df3927c09c0c137ea9cd7af0d716b594682f75f
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-01 14:49:12 +0530

    Time: 6 ms (57.88%) | Memory: 44.3 MB (71.37%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,37 +1,42 @@
 class Solution {
-    void dfs(HashMap<String,PriorityQueue<String>> adjList,String src, Stack<String> stack )
+    void dfs(String node, HashMap<String,PriorityQueue<String>> adjList,Stack<String> itinerary, int ticketsLeft)
     {
-
-        PriorityQueue<String> neighbourPQ =  adjList.get(src);
-        while(neighbourPQ!=null && !neighbourPQ.isEmpty())
+       
+        if(adjList.get(node)!=null)
         {
-            dfs(adjList,neighbourPQ.poll(),stack);
+        PriorityQueue<String> pq= adjList.get(node);
+        while(!pq.isEmpty())
+        {   
+            String adjNode= pq.poll();
+            dfs(adjNode,adjList,itinerary,ticketsLeft-1); 
         }
-        stack.push(src);
+        }
+        
+        itinerary.add(node);
     }
 
     public List<String> findItinerary(List<List<String>> tickets) {
+
         HashMap<String,PriorityQueue<String>> adjList = new HashMap<>();
-        List<String> validItinerary = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
-        int noOfTickets = tickets.size();
-        
-        for(int i=0;i<noOfTickets;i++)
+        int totalTickets = tickets.size();
+
+        for(int i=0;i<tickets.size();i++)
         {
             String src = tickets.get(i).get(0);
-            String dst = tickets.get(i).get(1);
-            if(!adjList.containsKey(src))adjList.put(src,new PriorityQueue<>());
-            adjList.get(src).add(dst);
+            String dest = tickets.get(i).get(1);
+            if(!adjList.containsKey(src)) adjList.put(src,new PriorityQueue<>());
+            adjList.get(src).add(dest);
         }
 
-        String src = "JFK";
-        dfs(adjList,src,stack);
+        Stack<String> itinerary = new Stack<>();
 
-        while(!stack.isEmpty())
-        {
-            validItinerary.add(stack.pop());
-        }
+        dfs("JFK",adjList,itinerary,totalTickets);
 
-        return validItinerary;
+        List<String> ans = new ArrayList<>();
+        while(!itinerary.isEmpty()) ans.add(itinerary.pop());
+
+        return ans;
+
+
     }
 }
