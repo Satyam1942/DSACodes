f96e4eca16bef62d67ec3e049708caf2f62df69a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-01 17:07:47 +0530

    Time: 49 ms (28.09%) | Memory: 96.8 MB (71.79%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,46 +1,35 @@
-class CustomComparator implements Comparator<int[]>
-{
-    public int compare(int[] a, int []b)
-    {
-        if(a[0]==b[0]) return b[1]-a[1];
-        else  return a[0] - b[0];
-    }
-}
-
 class Solution {
-    int findLowerBound(int h , List<Integer> lis)
+
+    int getFloorIndex(List<Integer> sorted, int height)
     {
         int left = 0;
-        int right = lis.size() -1;
-        int lowerBound = -1;
+        int right = sorted.size()-1;
+        int maxIndex = right+1;
         while(left<=right)
         {
             int mid = (left+right)/2;
-            if(h<=lis.get(mid)) right = mid-1;
-            else {left = mid+1; lowerBound = mid;}
+            if(sorted.get(mid)>=height) {right = mid-1; maxIndex= mid;}
+            else left = mid+1;
         }
 
-        return lowerBound+1;
+        return maxIndex;
     }
 
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes ,new CustomComparator());
-        int len = envelopes.length;
+        Arrays.sort(envelopes, (a,b)->((a[0]==b[0])?b[1]-a[1]:a[0]-b[0]));
+        List<Integer> sorted = new ArrayList<>();
+        //find Longest Increasing Subsequence wrt height using  Binary Search
 
-       List<Integer> lis = new ArrayList<>();
-        lis.add(envelopes[0][1]);
-
-        for(int i=1;i<len;i++)
+        sorted.add(envelopes[0][1]);
+        for(int i=1;i<envelopes.length;i++)
         {
             int height = envelopes[i][1];
-            if(height>lis.get(lis.size()-1)) lis.add(height);
-            else
-            {
-            int lowerBound = findLowerBound(height,lis);
-            lis.set(lowerBound,height);
-            }
+            int index = getFloorIndex(sorted,height);
+            if(index>=sorted.size()) sorted.add(height);
+            else sorted.set(index,height);
         }
+        
+        return sorted.size();
 
-        return lis.size();
     }
 }
