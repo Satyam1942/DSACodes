ab5644322c720fea7268fdfa21ee8b5a0ad37026
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-07 10:22:25 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (11.51%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -4,8 +4,7 @@ class Solution {
     for(int i=num.length()-1;i>=0;i--)
     {
             int digit= (int)num.charAt(i)-48;
-            if(digit%2!=0) return num;
-            else num = num.substring(0,i);
+            if(digit%2!=0) return num.substring(0,i+1);
     }
     return "";
     }
