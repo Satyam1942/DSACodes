216e45c89950fff3c8fb4f10628b5f9d9c323d80
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-10 23:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (75.52%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n =piles.length;
-        int  dp[][] = new int[n+1][n+1];
-
-        int i=0,j=n-1;
-        while(i<=j)
-        {  
-            
-            int takeFirst = piles[i]-dp[i+1][j];
-            int takeLast =  piles[j-1]-dp[i][j-1];
-            dp[i][j] = Math.max(takeFirst,takeLast);
-            i++;
-            j--;
-        }
-
-        return dp[0][piles.length-1]>0;
+        return true;
     }
 }
