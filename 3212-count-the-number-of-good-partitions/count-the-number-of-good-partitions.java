6628a41b862342fa89004cd17a30fc80cb16c6a1
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-11 10:30:23 +0530

    Time: 36 ms (50.00%) | Memory: 57 MB (50.00%) - LeetSync

--- 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
@@ -1,5 +1,5 @@
 class Solution {
-    boolean isPossible(int noOfPartitions, int []nums, HashMap<Integer,Integer> map)
+    int getMaxPartition(int []nums, HashMap<Integer,Integer> map)
     {
         int count = 0;
         int maxPIndex= 0;
@@ -12,34 +12,24 @@ class Solution {
             if(i>=maxPIndex)count++;
         } 
 
-        return (count-1)>=noOfPartitions;
+        return (count-1);
     }
 
     public int numberOfGoodPartitions(int[] nums) {
         
         int left = 0;
         int right = nums.length-1;
-        int maxPartitionAllowed = 0;
+
         HashMap<Integer,Integer> lastIndex =new HashMap<>();
         
-        for(int i=0;i<nums.length;i++)
-        {
-            lastIndex.put(nums[i],i);
-        }
+        for(int i=0;i<nums.length;i++) lastIndex.put(nums[i],i);
 
-        while(left<=right)
-        {
-            int mid= (left+right)/2;
-            if(isPossible(mid,nums,lastIndex)){left = mid+1; maxPartitionAllowed=mid;}
-            else right= mid-1;
-        }
+        int maxPartitionAllowed = getMaxPartition(nums,lastIndex);
         
         long ans = 1l;
         long mod= (long)(1e9+7);
-        for(int i=0;i<maxPartitionAllowed;i++)
-        {
-            ans= (ans*2)%mod;
-        }
+        for(int i=0;i<maxPartitionAllowed;i++) ans= (ans*2)%mod;
+        
         return (int)(ans);
     }
 }
