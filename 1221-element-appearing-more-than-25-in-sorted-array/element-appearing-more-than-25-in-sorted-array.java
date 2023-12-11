368de8b27a06a147849e3dab2d4aa5a413e6f5a8
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-11 09:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.42%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,23 +1,52 @@
 class Solution {
-    public int findSpecialInteger(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i=0;i<arr.length;i++)
+    
+    int findFirstOccurence(int arr[],int val)
+    {
+        int left = 0;
+        int right = arr.length-1;
+        int firstOcc = 0;
+        while(left<=right)
         {
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            int mid = (left+right)/2;
+            if(arr[mid]>=val){ firstOcc= mid; right= mid-1;}
+            else left = mid+1;
         }
-       
-       int maxi = Integer.MIN_VALUE;
-       int maxValue = -1;
-       for(Map.Entry<Integer,Integer> i:map.entrySet())
-       {
-           if(maxi<i.getValue())
-           {
-           maxi = i.getValue();
-           maxValue = i.getKey();
-           }
-       }
 
-       return maxValue;
+        return firstOcc;
+    }
+
+    int findLastOccurence(int arr[],int val)
+    {
+        int left = 0;
+        int right = arr.length-1;
+        int lastOcc = right;
+
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+            if(arr[mid]<=val){left = mid+1; lastOcc = mid;}
+            else right = mid-1;
+        }
+
+        return lastOcc;
+    }
+
+    public int findSpecialInteger(int[] arr) {
+        
+        int n = arr.length;
+        int mainEle[] = new int[3];
+        mainEle[0]= arr[n/4];
+        mainEle[1]= arr[n/2];
+        mainEle[2]= arr[3*n/4];
+
+        for(int i:mainEle)
+        {
+            int f = findFirstOccurence(arr,i);
+            int l = findLastOccurence(arr,i);
+            if(l-f+1>n/4) return i;
+        }
+        
+        return -1;
+               
     }
 }
