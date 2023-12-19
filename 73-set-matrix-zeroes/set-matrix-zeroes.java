bd2e42c611534db502710cd7691d3faf1e173c87
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-19 18:00:42 +0530

    Time: 1 ms (91.43%) | Memory: 44.4 MB (73.53%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,65 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean horizontal = false, vertical = false;
+
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0) vertical = true;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[0][i]==0) horizontal = true;
+        }
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++)
+        {
+           if(matrix[i][0]==0 && i!=0)
+           {
+               for(int j=0;j<n;j++)
+               {
+                   matrix[i][j] = 0;
+               }
+           }
+        }
+
+        for(int j=1;j<n;j++)
+        {
+           if(matrix[0][j]==0)
+           {
+               for(int i=0;i<m;i++)
+               {
+                   matrix[i][j] = 0;
+               }
+           }
+        }
+
+           if(vertical) for(int i=0;i<m;i++) matrix[i][0] = 0;
+            if(horizontal) for(int i=0;i<n;i++) matrix[0][i] = 0;
+        
+     
+    }
+}
+
+/*
+   0 0  3  0
+   0 0  0  0
+   0 0  0  0
+   0 0  0  0
+ */
