c4dccaa96b7508cd73344ab3b029079c4b095ff2
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-30 16:15:52 +0530

    Time: 5 ms (59.69%) | Memory: 47.2 MB (69.17%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    int noOfSubarraysGreaterThan(int nums[], int target) {
+        int length = nums.length;
+        int left = 0, right = 0;
+        int count = 0,sum = 0;
+
+        while (left <= right && right <= length) {
+            if (sum > target) {
+                count += length - right + 1;
+                sum -= nums[left];
+                left++;
+                continue;
+            }
+
+            if (right < length)
+               sum += nums[right];
+            right++;
+        }
+        return count;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int target) {
+        
+          if(target==0)
+       {
+           int length = nums.length; int count = 0, icr=1;
+           for(int i=0;i<length;i++)
+           {
+               if(nums[i]==0) {count+=icr; icr++;}
+               else icr = 1;
+           }
+           return count;
+       }
+
+      return noOfSubarraysGreaterThan(nums,target-1)-noOfSubarraysGreaterThan(nums,target);
+
+    }
+}
