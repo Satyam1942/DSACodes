12b3a6130a6a2eb8670955ee0b92b514454264f0
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2023-12-30 17:57:46 +0530

    Time: 1 ms (95.78%) | Memory: 41.1 MB (18.73%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.java
+++ 362-design-hit-counter/design-hit-counter.java
@@ -1,29 +1,21 @@
 class HitCounter {
 
-    TreeMap<Integer, Integer> map;
-    int prevTimestamp;
+    ArrayDeque<Integer> dq;
     int timeDur;
 
     public HitCounter() {
-        map = new TreeMap<>();
-        map.put(0,0);
-        prevTimestamp = 0;
+        dq = new ArrayDeque<>();
         timeDur = 300;
     }
 
     public void hit(int timestamp) {
-        int prevFreq = (!map.isEmpty()) ? map.get(prevTimestamp) : 0;
-        map.put(timestamp, prevFreq + 1);
-        prevTimestamp = timestamp;
+        dq.addLast(timestamp);
     }
 
     public int getHits(int timestamp) {
         int lowerBound = Math.max(timestamp - timeDur, 0);
-        int currHits = map.get(prevTimestamp);
-        Integer lowerBoundTimestamp = map.floorKey(lowerBound);
-        int lowerBoundHits = (lowerBoundTimestamp == null) ? currHits : map.get(lowerBoundTimestamp);
-
-        return currHits - lowerBoundHits;
+        while(!dq.isEmpty() && dq.peekFirst()<=lowerBound) dq.removeFirst();
+        return dq.size();
     }
 }
 
