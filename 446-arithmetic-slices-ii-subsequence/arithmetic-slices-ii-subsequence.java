4c28cc4494476c82e9328318d74e2367e28efdcf
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-01-07 13:31:10 +0530

    Time: 186 ms (42.44%) | Memory: 86.5 MB (81.98%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
@@ -3,29 +3,19 @@ class Solution {
         HashMap<Long,Integer>[] cache = new HashMap[nums.length];
         for(int i=0;i<nums.length;i++) cache[i] = new HashMap<>();
 
+           int count = 0;
         for(int i=0;i<nums.length;i++)
         {
             for(int j=0;j<i;j++)
             {
                 long dif = (long)nums[i]-(long)nums[j];
                 int freq = cache[i].getOrDefault(dif,0)+cache[j].getOrDefault(dif,0);
+                count+=cache[j].getOrDefault(dif,0);
                 cache[i].put(dif,freq+1);
             }
         }       
 
-        int count = 0;
+     return count;
 
-        for(int i=0;i<nums.length;i++)
-        {
-            HashMap<Long,Integer> map = cache[i];
-            for(Map.Entry<Long,Integer> j:map.entrySet())
-            {
-                long dif = j.getKey();
-                int freq = j.getValue();
-                count+=freq;
-            }
-        }
-        int n = nums.length;
-        return count-(n*(n-1))/2;
     }
 }
