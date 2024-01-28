fe7b24fd02b69a59cfcea8ff6074eb8f42c0a84c
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-01-28 22:52:37 +0530

    Time: 154 ms (21.10%) | Memory: 44.7 MB (64.74%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -1,51 +1,45 @@
-class NumMatrix {
-    int prefixSum[][];
-    public NumMatrix(int[][] matrix) {
+class Solution {
+    int[][] createPrefixMatrix(int matrix[][]){
         int m = matrix.length;
         int n = matrix[0].length;
-        prefixSum = new int[m+1][n+1];
-        for(int i=1;i<=m;i++)
-        {
-            int prefixRow =0;
-            for(int j=1;j<=n;j++)
-            {
-            prefixRow+=matrix[i-1][j-1];
-            prefixSum[i][j] = prefixRow + prefixSum[i-1][j];
+        int prefixMatrix[][] = new int[m+1][n+1];
+        
+        for(int i=0;i<m;i++){
+            int prefixSumRow = 0;
+            for(int j=0;j<n;j++){
+                prefixSumRow+=matrix[i][j];
+                prefixMatrix[i+1][j+1] = prefixSumRow+prefixMatrix[i][j+1];
             }
         }
-    }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefixSum[row2+1][col2+1]-prefixSum[row1][col2+1]
-                -prefixSum[row2+1][col1]+prefixSum[row1][col1];
-    }
-}
 
-class Solution {
+        return prefixMatrix;
+    }
+
+    int sum(int row1,int col1, int row2, int col2, int prefixMatrix[][]){
+        return prefixMatrix[row2+1][col2+1]-prefixMatrix[row1][col2+1] 
+                -prefixMatrix[row2+1][col1] + prefixMatrix[row1][col1];
+    }
+
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-      
-        int m = matrix.length;
-        int n = matrix[0].length;
+         int m = matrix.length;
+         int n = matrix[0].length;
 
-        NumMatrix sumClass = new NumMatrix(matrix);
-        int count =0;
-        for(int row1=0;row1<m;row1++)
-        {
-            for(int row2=row1;row2<m;row2++)
-            { 
-              HashMap<Integer,Integer> storeSumFreq = new HashMap<>();
-              storeSumFreq.put(0,1);
-               int col=0,tempSum=0;
-                while(col<n)
-                {
-                 tempSum += sumClass.sumRegion(row1,col,row2,col);
-                 count+=storeSumFreq.getOrDefault(tempSum-target,0);
-                 storeSumFreq.put(tempSum,storeSumFreq.getOrDefault(tempSum,0)+1);
-                 col++;
+        int prefixSum[][] = new int[m+1][n+1];
+        prefixSum = createPrefixMatrix(matrix);
+
+        long count = 0;
+        for(int row1=0;row1<m;row1++){
+            for(int row2=row1;row2<m;row2++){
+                HashMap<Integer,Integer> map = new HashMap<>();
+                map.put(0,1);
+                int tempSum = 0;
+                for(int col1=0;col1<n;col1++){
+                    tempSum += sum(row1,col1,row2,col1,prefixSum);
+                    count+= map.getOrDefault(tempSum-target,0);
+                    map.put(tempSum,map.getOrDefault(tempSum,0)+1);
                 }
             }
         }
-
-        return count;
+        return (int)count;
     }
 }
