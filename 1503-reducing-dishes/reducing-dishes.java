b7b1185d3e4b699aa7534f459c8f89e1836f2aa4
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-02-01 14:29:23 +0530

    Time: 3 ms (62.37%) | Memory: 41.7 MB (63.07%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -3,27 +3,21 @@ class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         int length = satisfaction.length;
         Arrays.sort(satisfaction);
-        Integer suffixSum[] = new Integer[length];
 
         int sum = 0;
-        for (int i = 0; i < length; i++)
-            sum += satisfaction[i];
-
         int prevLikeCoefficient = 0;
-
-        for (int i = 0; i < length; i++) {
-            suffixSum[i] = sum;
-            sum -= satisfaction[i];
+        for (int i = 0; i < length; i++){
+            sum += satisfaction[i];
             prevLikeCoefficient += satisfaction[i] * (i + 1);
         }
 
         int maxLikeCoefficient = Math.max(0,prevLikeCoefficient);
 
         for (int i = 1; i < length; i++) {
-            int curLikeCoefficient = prevLikeCoefficient - suffixSum[i - 1];
+            int curLikeCoefficient = prevLikeCoefficient - sum;
             maxLikeCoefficient = Math.max(maxLikeCoefficient,
                     curLikeCoefficient);
-
+            sum-=satisfaction[i-1];
             prevLikeCoefficient = curLikeCoefficient;
         }
 
