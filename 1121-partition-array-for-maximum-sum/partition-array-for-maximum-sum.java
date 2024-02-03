4007360d61aa9ef86126d08eda7d5d28096e4658
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-02-03 08:35:47 +0530

    Time: 5 ms (99.61%) | Memory: 41.9 MB (60.05%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,28 +1,22 @@
 class Solution {
-    int partitionArrayMaxSum(int index, int arr[], int k, Integer cache[]) {
+    //TABULATION APPRPOACH
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int cache[] = new int[arr.length + 1];
 
-        if (index == arr.length)
-            return 0;
+        for (int index = arr.length - 1; index >= 0; index--) {
+            int finalIndex = Math.min(arr.length - 1, index + k - 1);
+            int maxi = 0;
+            int maxElement = 0;
 
-        if (cache[index] != null)
-            return cache[index];
+            for (int i = index; i <= finalIndex; i++) {
+                maxElement = Math.max(maxElement, arr[i]);
+                int temp = maxElement * (i - index + 1) + cache[i + 1];
+                maxi = Math.max(maxi, temp);
+            }
 
-        int finalIndex = Math.min(arr.length - 1, index + k-1);
-        int maxi = 0;
-        int maxElement = 0;
-
-        for (int i = index; i <= finalIndex; i++) {
-            maxElement = Math.max(maxElement, arr[i]);
-            int temp = maxElement * (i - index + 1) +
-                    partitionArrayMaxSum(i + 1, arr, k,cache);
-            maxi = Math.max(maxi, temp);
+            cache[index] = maxi;
         }
 
-        return cache[index] = maxi;
-    }
-
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        Integer cache[]= new Integer[arr.length];
-        return partitionArrayMaxSum(0,arr,k,cache);
+        return cache[0];
     }
 }
