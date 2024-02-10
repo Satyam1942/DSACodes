df3fb7c09f79ded6239946fd36f1a48732ba6240
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-02-10 07:54:22 +0530

    Time: 3 ms (67.98%) | Memory: 41.5 MB (61.97%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,13 +1,13 @@
 class Solution {
 
-    void computeOddLengthPalindromes(String s, int cache[][]) {
+    void computeOddLengthPalindromes(String s, int count[]) {
         int length = s.length();
         for (int center = 0; center < length; center++) {
             int left = center;
             int right = center;
             while (left >= 0 && right < length) {
                 if (s.charAt(left) == s.charAt(right))
-                    cache[left][right]++;
+                    count[0]++;
                 else
                     break;
                 left--;
@@ -16,14 +16,14 @@ class Solution {
         }
     }
 
-    void computeEvenLengthPalindromes(String s, int cache[][]) {
+    void computeEvenLengthPalindromes(String s, int count[]) {
         int length = s.length();
         for (int center = 0; center < length - 1; center++) {
             int left = center;
             int right = center + 1;
             while (left >= 0 && right < length) {
                 if (s.charAt(left) == s.charAt(right))
-                    cache[left][right]++;
+                    count[0]++;
                 else
                     break;
                 left--;
@@ -34,19 +34,12 @@ class Solution {
 
     public int countSubstrings(String s) {
         int length = s.length();
-        int cache[][] = new int[length][length];
+        int count[] = new int[1];
 
-        computeOddLengthPalindromes(s, cache);
-        computeEvenLengthPalindromes(s, cache);
+        computeOddLengthPalindromes(s, count);
+        computeEvenLengthPalindromes(s, count);
 
-        int count = 0;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                count += cache[i][j];
-            }
-        }
-
-        return count;
+        return count[0];
 
     }
 }
