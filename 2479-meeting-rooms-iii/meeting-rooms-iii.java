a22404a427b70d78a6a389e9dd5d3e4abe51a34e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-02-18 12:59:50 +0530

    Time: 125 ms (12.62%) | Memory: 102.1 MB (6.06%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,93 +1,75 @@
-class Pair
-{
-    int start,end,roomNo;
-    Pair(int start, int end,int roomNo)
-    {
-        this.start = start;
+class Pair {
+    int end, room;
+
+    Pair(int end, int room) {
+        this.room = room;
         this.end = end;
-        this.roomNo = roomNo;
+    }
+}
+
+class CustomComparator implements Comparator<Pair> {
+    public int compare(Pair a, Pair b) {
+        if (a.end == b.end)
+            return a.room - b.room;
+        else
+            return a.end - b.end;
     }
 }
 
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
+        PriorityQueue<int[]> meetingTimes = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        PriorityQueue<Pair> scheduled = new PriorityQueue<>(new CustomComparator());
+        PriorityQueue<Integer> freeRoom = new PriorityQueue<>();
+
+        int noOfMeetingsAssigned[] = new int[n];
         int noOfMeetings = meetings.length;
-        Arrays.sort(meetings,(a,b)->(a[0]-b[0]));
+        for (int i = 0; i < noOfMeetings; i++)
+            meetingTimes.add(meetings[i]);
 
-    int meetingNo[] = new int[n];
-   PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->((a.end==b.end)?(a.roomNo-b.roomNo):a.end-b.end));
-   PriorityQueue<Integer>  freeRoom = new PriorityQueue<>();
-   for(int i=0;i<n;i++) freeRoom.add(i);
+        for (int i = 0; i < n; i++)
+            freeRoom.add(i);
 
-    pq.add(new Pair(meetings[0][0],meetings[0][1],0));
-    freeRoom.poll();
-    meetingNo[0]++;
+        while (!meetingTimes.isEmpty()) {
+            int curStart = meetingTimes.peek()[0];
+            int curEnd = meetingTimes.peek()[1];
+            meetingTimes.poll();
+            int prevEnd = (!scheduled.isEmpty()) ? scheduled.peek().end : 0;
 
-    for(int i=1;i<meetings.length;i++)
-    {
-       int curRoom = pq.peek().roomNo;
-       int prevStart = pq.peek().start;
-       int prevEnd = pq.peek().end;
-       int curStart = meetings[i][0];
-       int curEnd = meetings[i][1];
-       
-       if(prevEnd>curStart)
-       {
-           if(freeRoom.isEmpty())
-           {
-               pq.poll();
-            int newStart = prevEnd;
-            int newEnd = newStart+(curEnd-curStart);
-            pq.add(new Pair(newStart,newEnd,curRoom));
-            meetingNo[curRoom]++;
-           }
-           else
-           {
-           int newRoom = freeRoom.poll();
-           meetingNo[newRoom]++;
-           pq.add(new Pair(curStart,curEnd,newRoom));
-           }
-       }
-       else
-       {
-   
-           while(!pq.isEmpty() && pq.peek().end<=curStart)
-           { 
-               freeRoom.add(pq.peek().roomNo);
-               pq.poll();
-           }
-          
-           int newRoom = (!freeRoom.isEmpty())?freeRoom.poll():curRoom;
-           meetingNo[newRoom]++;
-           pq.add(new Pair(curStart,curEnd,newRoom)); 
-       }
+            if (freeRoom.isEmpty()) {
 
-    }
+                if (curStart >= prevEnd) {
+                    while (!scheduled.isEmpty() && curStart >= scheduled.peek().end)
+                        freeRoom.add(scheduled.poll().room);
 
-    int maxi = 0, maxMeetingRoom = -1;
-    for(int i=0;i<n;i++)
-    {
-        if(meetingNo[i]>maxi) {maxi = meetingNo[i]; maxMeetingRoom = i;}
-    }
+                    int roomNo = freeRoom.poll();
+                    scheduled.add(new Pair(curEnd, roomNo));
+                    noOfMeetingsAssigned[roomNo]++;
+                } else {
+                    int roomNo = scheduled.poll().room;
+                    int newEnd = prevEnd+(curEnd-curStart);
+                    scheduled.add(new Pair(newEnd, roomNo));
+                    noOfMeetingsAssigned[roomNo]++;
+                }
 
-    return maxMeetingRoom;
+            } else {
+
+                while (!scheduled.isEmpty() && curStart >= scheduled.peek().end)
+                    freeRoom.add(scheduled.poll().room);
+                int roomNo = freeRoom.poll();
+                scheduled.add(new Pair(curEnd, roomNo));
+                noOfMeetingsAssigned[roomNo]++;
+            }
+        }
+
+        int maxMeetings = 0, maxRoom = 0;
+        for (int roomNo = 0; roomNo < n; roomNo++) {
+            if (maxMeetings < noOfMeetingsAssigned[roomNo]) {
+                maxMeetings = noOfMeetingsAssigned[roomNo];
+                maxRoom = roomNo;
+            }
+        }
+
+        return maxRoom;
     }
 }
-
-/*
-    0 1
-    PriorityQueue for rooms
-    PriorityQueue for meeting Times = startTimes
-
-    0     [0,10]       [1,1]
-    1     [1,5]         (0,10)
-          [5,10]        (1,5)
-          [10,11]
-
-        [0,10]
-        [1,5]
-
-        0-1
-        1-2
-          Step1 : Schedule the meetings
-  */
