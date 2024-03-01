74fd43162f1fbfa16df0feb62664dec01ac68058
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-03-01 19:04:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (55.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,43 +1,29 @@
 class Solution {
-   int getPivotIndex(int arr[])
-    {
+    public int search(int[] nums, int target) {
         int left = 0;
-        int right = arr.length-1;
-        int pivot = 0;
-
-        while(left<right)
-        {
+        int right = nums.length-1;
+        while(left<=right){
             int mid = (left+right)/2;
-            if(arr[mid]>arr[right]) left = mid+1;
-            else if(arr[mid]<=arr[right])
-            {
-                right = mid;
-                pivot = mid;
+            if(target==nums[mid])
+                return mid;
+
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<nums[mid])
+                    right = mid-1;
+                else 
+                    left = mid+1;
+
+            }else {
+                if(target>nums[mid] && target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
             }
         }
 
-        pivot = right;
-        return pivot;
-    }
-
-    int binarySearch(int arr[] , int left ,int right, int val)
-    {
-        while(left<=right)
-        {
-            int mid = (left+right)/2;
-            if(arr[mid]>val) right = mid-1;
-            else if(arr[mid]<val) left = mid+1;
-            else return mid;
-        }
         return -1;
     }
-
-
-    public int search(int[] nums, int target) {
-        int k = getPivotIndex(nums);
-        System.out.println(k);
-        int indx1 = binarySearch(nums,0,k-1, target) ;
-        int indx2 = binarySearch(nums,k,nums.length-1, target) ;
-       return Math.max(indx1,indx2);
-    }
 }
+/*
+    [...,5,6,7,8,0,1,2]
+ */
