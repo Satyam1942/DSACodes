5c483e545fdd15dae4edeb92d3801c0948d7ef1f
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-03-15 16:12:34 +0530

    Time: 6 ms (73.36%) | Memory: 45.2 MB (59.27%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,42 +1,43 @@
 class Solution {
-    void dfs(String node, HashMap<String,PriorityQueue<String>> adjList,Stack<String> itinerary)
-    {
-       
-        if(adjList.get(node)!=null)
-        {
-        PriorityQueue<String> pq= adjList.get(node);
-        while(!pq.isEmpty())
-        {   
-            String adjNode= pq.poll();
-            dfs(adjNode,adjList,itinerary); 
+    void buildGraph(List<List<String>> tickets, HashMap<String, PriorityQueue<String>> adjList) {
+        int numberOfTickets = tickets.size();
+        for (int i = 0; i < numberOfTickets; i++) {
+            String from = tickets.get(i).get(0);
+            String to = tickets.get(i).get(1);
+
+            if (!adjList.containsKey(from))
+                adjList.put(from, new PriorityQueue<>());
+            adjList.get(from).add(to);
         }
+    }
+
+    void dfs(String curNode, HashMap<String,PriorityQueue<String>> adjList, Stack<String> reverseItinerary ){
+     
+        if(adjList.containsKey(curNode)){
+            PriorityQueue<String> pq = adjList.get(curNode); 
+            while(!pq.isEmpty()){
+                String adjNode = pq.poll();
+                dfs(adjNode,adjList,reverseItinerary);
+            }
         }
-        
-        itinerary.add(node);
+
+        reverseItinerary.push(curNode);
     }
 
     public List<String> findItinerary(List<List<String>> tickets) {
+        HashMap<String, PriorityQueue<String>> adjList = new HashMap<>();
+        buildGraph(tickets, adjList);
 
-        HashMap<String,PriorityQueue<String>> adjList = new HashMap<>();
-        
+        Stack<String> reverseItinerary = new Stack<>();
+        String startNode = "JFK";
+        dfs(startNode, adjList, reverseItinerary);
 
-        for(int i=0;i<tickets.size();i++)
-        {
-            String src = tickets.get(i).get(0);
-            String dest = tickets.get(i).get(1);
-            if(!adjList.containsKey(src)) adjList.put(src,new PriorityQueue<>());
-            adjList.get(src).add(dest);
+        List<String> itinerary = new ArrayList<>();
+
+        while (!reverseItinerary.isEmpty()) {
+            itinerary.add(reverseItinerary.pop());
         }
 
-        Stack<String> itinerary = new Stack<>();
-
-        dfs("JFK",adjList,itinerary);
-
-        List<String> ans = new ArrayList<>();
-        while(!itinerary.isEmpty()) ans.add(itinerary.pop());
-
-        return ans;
-
-
+        return itinerary;
     }
 }
