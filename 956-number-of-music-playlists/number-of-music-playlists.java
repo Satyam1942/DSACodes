eb70d7014ea47c39a1130ac3a087c284c76ecd46
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-03-15 17:16:56 +0530

    Time: 1 ms (100.00%) | Memory: 41.7 MB (84.88%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.java
+++ 956-number-of-music-playlists/number-of-music-playlists.java
@@ -1,27 +1,32 @@
 class Solution {
     long mod = (long)(1e9+7);
-
-    long rec(int n, int goal,int k,int oldSongs, Long dp[][])
-    {
-        if(goal==0) 
-        {
-            if(oldSongs==n) return  1; 
-            else return 0;
+    long calculateMusicPlaylist(int oldSongs, int goal, int n, int k, Long cache[][]) {
+        if (goal == 0) {
+            if (oldSongs == n)
+                return 1;
+            else
+                return 0;
         }
+
+        if(cache[oldSongs][goal]!=null)
+            return cache[oldSongs][goal];
+
+        long pickOldSong = 0;
+        long pickNewSong = 0;
+
+        if(oldSongs>=k)
+            pickOldSong =  (oldSongs-k)*calculateMusicPlaylist(oldSongs, goal - 1, n, k,cache);
+        if(oldSongs<n)
+            pickNewSong =  (n-oldSongs)*calculateMusicPlaylist(oldSongs+1, goal - 1, n, k,cache);
         
-        if(dp[goal][oldSongs]!=null) return dp[goal][oldSongs];
-
-        long playOld =0l, playNew = 0l;
-
-       if(oldSongs>k)  playOld = (oldSongs-k)*rec(n,goal-1,k,oldSongs,dp);
-       if(n>oldSongs) playNew = (n-oldSongs)*rec(n,goal-1,k,oldSongs+1,dp);
-
-       return dp[goal][oldSongs] = (playOld+playNew)%mod;
+        return cache[oldSongs][goal] = (pickOldSong%mod+pickNewSong%mod)%mod;
     }
 
     public int numMusicPlaylists(int n, int goal, int k) {
-        Long dp[][] = new Long[goal+1][n+1];
+        Long cache[][] = new Long[n+1][goal+1];
 
-        return (int)rec(n,goal,k,0,dp);
+        int noOfPlaylists =  (int)calculateMusicPlaylist(0,goal,n,k,cache);
+        
+        return noOfPlaylists;
     }
 }
