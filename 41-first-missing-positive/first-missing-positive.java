8bffe04289420b7c7149baf9a5cf12cff92d2588
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-03-26 07:24:56 +0530

    Time: 1 ms (98.00%) | Memory: 55.4 MB (30.89%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,28 +1,42 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-    
+    void initialiseNegativeNumbersAsZero(int nums[]){
         int len = nums.length;
-         for(int i=0;i<len;i++)
-         {
-             if(nums[i]<0)nums[i]=0;
-         }
+        for(int i=0;i<len;i++) {
+            if(nums[i]<0)
+                nums[i]=0;
+        }
+    }
 
-         for(int i=0;i<len;i++)
-         {
+    int getFirstNonNegativeNumber(int nums[]){
+         int len = nums.length;
+
+         for(int i=1;i<=len;i++) {
+            if(nums[i-1]>=0) 
+                return i;
+         }
+        return len+1;
+    }
+
+    public int firstMissingPositive(int[] nums) {
+
+         int len = nums.length;
+
+        initialiseNegativeNumbersAsZero(nums);
+
+         for(int i=0;i<len;i++) {
              int index = Math.abs(nums[i])-1;
              if(index>=0 && index<len && nums[index]>=0)
              {
-             if(nums[index]==0) nums[index]= -(len+1);
-               else  nums[index]= -nums[index];
+                if(nums[index]==0) 
+                    nums[index]= -(len+1);
+                else  
+                    nums[index]= -nums[index];
              }
          }
 
 
-         for(int i=1;i<=len;i++)
-         {
-             if(nums[i-1]>=0) return i;
-         }
+        int firstMissingPositive = getFirstNonNegativeNumber(nums);
 
-        return len+1;
+        return firstMissingPositive;
     }
 }
