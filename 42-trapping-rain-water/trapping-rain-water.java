6e67ea1739a7ac5325bbff44e6e10b695539e3a4
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-04-12 22:17:41 +0530

    Time: 1 ms (71.17%) | Memory: 46.1 MB (55.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,43 +1,36 @@
 class Solution {
     public int trap(int[] height) {
-        int len = height.length;
-        int left=0,right=1;
-        int count=0;
-        int forward[] = new int[len];
-        int backward[] = new int[len];
+        int width = height.length;
+        int forward[] = new int[width];
+        int backward[] = new int[width];
 
-        while(right<len)
-        {
-            if(height[right]<height[left])
-            {
-                forward[right]=height[left]-height[right];
-            }
-            else
-            {
+        int left = 0, right = 1;
+        while(right<width){
+            if(height[left]>height[right]){
+                forward[right] = height[left]-height[right];
+            }else{
                 left = right;
             }
             right++;
-        }
+        }   
 
-        right = len-2;
-        left =len-1;
-        while(right>=0)
-        {
-            if(height[right]<height[left])
-            {
-                backward[right]=height[left]-height[right];
-            }
-            else
-            {
+        left = width-1;
+        right = width-2;
+        while(right>=0){
+            if(height[left]>height[right]){
+                backward[right] = height[left]-height[right];
+            }else{
                 left = right;
             }
             right--;
         }
 
-        for(int i=0;i<len;i++)
-        {
-            count+= Math.min(forward[i],backward[i]);
+        int totalRainWater = 0;
+        for(int i=0;i<width;i++){
+            totalRainWater+= Math.min(forward[i],backward[i]);
         }
-        return count;
+
+        return totalRainWater;
+
     }
 }
