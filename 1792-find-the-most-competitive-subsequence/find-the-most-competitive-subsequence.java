62665bb4642335d583ab93e211331ccd38ee3d2e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-04-19 22:10:44 +0530

    Time: 21 ms (88.07%) | Memory: 62.6 MB (20.81%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -1,23 +1,42 @@
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
-
+        int mostCompetitveSubsequence[] = new int[k];
         int n = nums.length;
-        int res[] = new int[k];
-        Stack<Integer> st = new Stack<>();
+        ArrayDeque<Integer> adq = new ArrayDeque<>();
 
-        for(int i=0;i<n;i++)
-        {
-            while(!st.isEmpty() && nums[i]<st.peek() && nums.length-i+st.size()>k) st.pop();
-            if(st.size()<k) st.push(nums[i]); 
+        for(int i=0;i<=n-k;i++){
+            while(!adq.isEmpty() && adq.peekLast()>nums[i]){
+                adq.pollLast();
+            }
+            adq.addLast(nums[i]);
         }
-        
-        for(int i=k-1;i>=0;i--) res[i] = st.pop();
 
-        return res;
+        int pointer = 0;
+        for(int i=n-k+1;i<n;i++){
+            mostCompetitveSubsequence[pointer++] = adq.pollFirst();
+            while(!adq.isEmpty() && adq.peekLast()>nums[i])
+                adq.pollLast();
+            adq.addLast(nums[i]);
+        }
+
+        mostCompetitveSubsequence[pointer++] = adq.pollFirst();
+        return  mostCompetitveSubsequence;
     }
 }
 
-
 /*
-Use monotonic stack with keyCondition: pop only if you can get array of size k in future 
- */
+    2,3,3,4
+   
+
+   2,3,3,4
+
+   0 to n-k add all numbers in monotonic form in array deque
+   then poll element from bottom add to the answer 
+   and then add one by one to array deque
+
+   6
+   4
+   3
+   3
+   2
+*/
