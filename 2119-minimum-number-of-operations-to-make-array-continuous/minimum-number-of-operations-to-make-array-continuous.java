d554db5159e3e07f52f083f6aeae6daf1dc3dc92
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-04-19 20:41:16 +0530

    Time: 42 ms (55.15%) | Memory: 63.4 MB (16.99%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.java
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.java
@@ -1,19 +1,48 @@
 class Solution {
     public int minOperations(int[] nums) {
-      Arrays.sort(nums);
-      int len = nums.length;
-      int count = len, j=1, dup = 0;
-      int dupArr[] = new int[len];
-        for(int i=0;i<len;i++)
-        {
-           for(;j<len && nums[j]<=nums[i]+len-1;j++)
-           {
-               if(nums[j]==nums[j-1]) dup++;
-               dupArr[j] = dup;
-           }
-            count = Math.min(count, len-(j-i) + dup-dupArr[i]);
+        int originalLength = nums.length;
+        
+        Arrays.sort(nums);
+
+        List<Integer> numsWithoutDuplicates = new ArrayList<>();
+        int prev = nums[0];
+        numsWithoutDuplicates.add(prev);
+
+        //creating new array by removing duplicates
+        for(int i=1;i<originalLength;i++){
+            if(nums[i]==prev)
+                continue;
+            else{
+                numsWithoutDuplicates.add(nums[i]);
+                prev = nums[i];
+            }
         }
 
-        return count;
-    }   
+        int leftPointer=0,rightPointer=0;
+        int minimumReplacements = originalLength;
+
+        int maxNumber = numsWithoutDuplicates.get(leftPointer)+nums.length-1;
+     
+        while(leftPointer<=rightPointer && rightPointer<numsWithoutDuplicates.size()) {
+            if(numsWithoutDuplicates.get(rightPointer)>maxNumber){
+                int windowLength = rightPointer-leftPointer;
+                minimumReplacements = Math.min(minimumReplacements, originalLength-windowLength);
+                leftPointer++;
+                maxNumber = numsWithoutDuplicates.get(leftPointer)+nums.length-1;
+                continue;
+            }
+            rightPointer++;
+        }
+
+        int windowLength = rightPointer-leftPointer;
+        minimumReplacements = Math.min(minimumReplacements, originalLength-windowLength);
+        
+        return minimumReplacements;
+    }
 }
+
+/*
+    Couple of Hints:
+    Try to use sliding window for your advantage
+    Create a new array without duplicates to avoid duplicates
+ */
