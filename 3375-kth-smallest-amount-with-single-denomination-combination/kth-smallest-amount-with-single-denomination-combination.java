b1797207fb6541eabe7d9eb703d353f6224b3fde
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-04-21 14:59:30 +0530

    Time: 33 ms (54.40%) | Memory: 44.4 MB (18.66%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,57 @@
+class Solution {
+    long gcd(long a,long b){
+        long ans = Math.min(a,b);
+        long num = Math.max(a,b);
+        while(ans>0){
+            long remainder = num%ans;
+            num = ans;
+            ans = remainder;
+        }
+
+        return num;
+    }
+
+    long getLCM(long a,long b){
+        return a*b/gcd(a,b);
+    }
+
+    boolean isPossible(long mid, long k, List<Long> lcm){
+   
+        long count = 0;
+        for(long i:lcm){
+            count+= mid/i;
+        }
+        return count>=k;
+    }
+
+    public long findKthSmallest(int[] coins, int k) {
+        Arrays.sort(coins);
+        int length = coins.length;
+
+        List<Long> lcm = new ArrayList<>();
+        for(int i=1;i<(1<<length);i++){
+            long l = 1;
+            for(int j=0;j<length;j++){
+                if((i&(1<<j)) >0){
+                    l = getLCM(l,(long)coins[j]);
+                }
+            }
+            lcm.add(l * (Integer.bitCount(i) % 2 == 1 ? 1 : -1));
+        }
+
+        long lowerBound  = 1;
+        long upperBound =  Long.MAX_VALUE;
+        long ans = upperBound;
+        while(lowerBound<=upperBound){
+            long mid = lowerBound+(upperBound-lowerBound)/2l;
+            if(isPossible(mid,k,lcm)){
+                ans = mid;
+                upperBound = mid-1;
+            }else{
+                lowerBound = mid+1;
+            }
+        }
+        return ans;
+        
+    }
+}
