c064ec1d2ecb137c2a1d0f83f794e3052f034949
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-04-26 06:59:00 +0530

    Time: 2 ms (99.32%) | Memory: 49.7 MB (73.13%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,26 +1,31 @@
 class Solution {
-    int fallingPathSum(int curRow, int prevCol, int [][] grid, Integer cache[][]){
-        if(curRow==grid.length)
-            return 0;
-        if(cache[curRow][prevCol+1]!=null)
-            return cache[curRow][prevCol+1];
+    public int minFallingPathSum(int[][] grid) {
+        int length = grid.length;
+        for(int i=0;i<length-1;i++){
+            int min = Integer.MAX_VALUE;
+            int minCol = 0;
+            int secondMin = Integer.MAX_VALUE;
+            for(int j=0;j<length;j++){
+                if(min>grid[i][j]){
+                    secondMin = min;
+                    min = grid[i][j];
+                    minCol = j;
+                }else if(secondMin>grid[i][j])
+                    secondMin = grid[i][j];
+            }
 
-        int sum = Integer.MAX_VALUE;
-        for(int i=0;i<grid[0].length;i++){
-            if(i==prevCol)
-                continue;
-            int temp = grid[curRow][i]+fallingPathSum(curRow+1,i,grid,cache);
-            sum = Math.min(sum,temp);
+            for(int j=0;j<length;j++){
+                if(j==minCol)
+                    grid[i+1][j] += secondMin;
+                else
+                    grid[i+1][j] += min;
+            }
         }
 
-        return cache[curRow][prevCol+1] =  sum;
-    }
-    public int minFallingPathSum(int[][] grid) {
-        Integer cache[][] = new Integer[grid.length][grid[0].length+1];
-        return fallingPathSum(0,-1,grid,cache);
+        int minSum = Integer.MAX_VALUE;
+        for(int j=0;j<length;j++)
+            minSum = Math.min(minSum,grid[length-1][j]);
+
+        return minSum;
     }
 }
-
-/*
-
- */
