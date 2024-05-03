438a7417f6330cfb49f529ff904e07ee9cbbd1d3
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-03 20:06:07 +0530

    Time: 1 ms (78.87%) | Memory: 41.1 MB (91.28%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,10 +1,36 @@
 class Solution {
+    String[] split(String v, char parameter) {
+        int length = v.length();
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char token = v.charAt(i);
+            if (token == parameter) {
+                list.add(sb.toString());
+                sb = new StringBuilder();
+            } else {
+                sb.append(token);
+            }
+        }
+
+        list.add(sb.toString());
+
+        String arr[] = new String[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
     public int compareVersion(String version1, String version2) {
-        String revisionVersion1[] = version1.split("\\.");
-        String revisionVersion2[] = version2.split("\\.");
+        String revisionVersion1[] = split(version1, '.');
+        String revisionVersion2[] = split(version2, '.');
+
         int noOfRevisions = Math.max(revisionVersion1.length, revisionVersion2.length);
 
         for (int i = 0; i < noOfRevisions; i++) {
+
             int rev1 = (i < revisionVersion1.length) ? Integer.valueOf(revisionVersion1[i]) : 0;
             int rev2 = (i < revisionVersion2.length) ? Integer.valueOf(revisionVersion2[i]) : 0;
 
@@ -14,7 +40,6 @@ class Solution {
                 return 1;
             else
                 return -1;
-
         }
 
         return 0;
