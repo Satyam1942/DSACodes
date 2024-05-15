ce636d9d281683635731d8ce4a6f5da889c9fc62
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-15 07:36:04 +0530

    Time: 170 ms (42.21%) | Memory: 67.5 MB (88.12%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -5,13 +5,13 @@ class Pair {
         this.row = row;
         this.col = col;
     }
-
 }
 
 class Solution {
-    void distanceFromAllThieves(int grid[][], int distFromThief[][]) {
-        int length = grid.length;
-        int width = grid[0].length;
+    // multisource bfs to determine distance of each cell from all thieves
+    void distanceFromAllThieves(List<List<Integer>> grid, int distFromThief[][]) {
+        int length = grid.size();
+        int width = grid.get(0).size();
 
         for (int row[] : distFromThief)
             Arrays.fill(row, Integer.MAX_VALUE);
@@ -20,7 +20,7 @@ class Solution {
 
         for (int row = 0; row < length; row++) {
             for (int col = 0; col < width; col++) {
-                if (grid[row][col] == 1) {
+                if (grid.get(row).get(col) == 1) {
                     q.add(new Pair(row, col));
                     distFromThief[row][col] = 0;
                 }
@@ -40,18 +40,20 @@ class Solution {
                 boolean isValid = adjRow >= 0 && adjCol >= 0 &&
                         adjRow < length && adjCol < width;
 
-                if (isValid && curDist + 1 < distFromThief[adjRow][adjCol]) {
-                    distFromThief[adjRow][adjCol] = curDist+ 1;
+                if (isValid && distFromThief[curRow][curCol] + 1 < distFromThief[adjRow][adjCol]) {
+                    distFromThief[adjRow][adjCol] = distFromThief[curRow][curCol] + 1;
                     q.add(new Pair(adjRow, adjCol));
                 }
             }
         }
     }
 
+    // djikstra’s algo to get safest path
     void safestPathInGrid(int distFromThief[][], int safeness[][]) {
         int length = distFromThief.length;
         int width = distFromThief[0].length;
-      	for(int row[]: safeness) Arrays.fill(row,Integer.MIN_VALUE);
+        for (int row[] : safeness)
+            Arrays.fill(row, Integer.MIN_VALUE);
 
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(0, 0));
@@ -69,38 +71,27 @@ class Solution {
                 int adjCol = curCol + delCol[i];
                 boolean isValid = adjRow >= 0 && adjCol >= 0 &&
                         adjRow < length && adjCol < width;
-
-                if (isValid ) {
+                if (isValid) {
                     int adjSafeness = distFromThief[adjRow][adjCol];
-                    if(safeness[adjRow][adjCol]<Math.min(curSafeness,adjSafeness))
-			        {
-					safeness[adjRow][adjCol] = Math.min(curSafeness,adjSafeness);
-					q.add(new Pair(adjRow,adjCol));
+                    if (safeness[adjRow][adjCol] < Math.min(curSafeness, adjSafeness)) {
+                        safeness[adjRow][adjCol] = Math.min(curSafeness, adjSafeness);
+                        q.add(new Pair(adjRow, adjCol));
                     }
                 }
             }
+
         }
 
     }
 
-
-    public int maximumSafenessFactor(List<List<Integer>> matrix) {
-
-        int grid[][] = new int[matrix.size()][matrix.size()];
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix.size(); j++) {
-                grid[i][j] = matrix.get(i).get(j);
-            }
-        }
-
-        int length = grid.length;
-        int width = grid[0].length;
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int length = grid.size();
+        int width = grid.get(0).size();
         int distFromThief[][] = new int[length][width];
         int safeness[][] = new int[length][width];
 
         distanceFromAllThieves(grid, distFromThief);
         safestPathInGrid(distFromThief, safeness);
-       
 
         int maxmSafeDist = safeness[length - 1][width - 1];
         return maxmSafeDist;
