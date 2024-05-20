5fc83d0700c5b126824274aeefc463fb03c9619e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-20 13:15:43 +0530

    Time: 168 ms (5.39%) | Memory: 44.2 MB (95.17%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,51 @@
+class Solution {
+    int noOfChangesRequired(String answerKey, int maxLength, char token){
+        int answerKeyLength = answerKey.length();
+        int noOfChanges = 0;
+        int left = 0, right = maxLength-1;
+
+        for(int i=left;i<=right;i++){
+            char ans = answerKey.charAt(i);
+            if(ans==token)
+                 noOfChanges++;
+        }
+   
+        int minChanges = noOfChanges;
+        while(right<answerKeyLength){
+            if(answerKey.charAt(left)==token)
+                noOfChanges--;
+            left++;
+            right++;
+            if(right<answerKeyLength){
+                if(answerKey.charAt(right)==token)
+                    noOfChanges++;
+                minChanges = Math.min(minChanges,noOfChanges);
+            }
+        }
+        return minChanges;
+    }
+
+    boolean isPossible(int maxChanges, int maxLength, String answerKey){
+        int noOfChangesTrue = noOfChangesRequired(answerKey,maxLength,'F');
+        int noOfChangesFalse = noOfChangesRequired(answerKey,maxLength,'T');
+
+        return maxChanges>=Math.min(noOfChangesTrue,noOfChangesFalse);
+    }
+
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+       int length = answerKey.length();
+       int left = 1;
+       int right =  length;
+       int ans = left;
+       while(left<=right){
+            int mid = (left+right)/2;
+            if(isPossible(k,mid,answerKey)){
+                ans = mid;
+                left = mid+1;
+            }else
+                right = mid-1;
+       }
+
+       return ans;
+    }
+}
