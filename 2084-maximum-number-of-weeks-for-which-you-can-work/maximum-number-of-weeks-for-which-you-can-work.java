17864b3d383c03fd3707f7f7c9882d5234889f4a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-20 12:44:40 +0530

    Time: 2 ms (100.00%) | Memory: 55.7 MB (89.42%) - LeetSync

--- 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.java
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.java
@@ -1,34 +1,28 @@
 class Solution {
     public long numberOfWeeks(int[] milestones) {
-        if(milestones.length==1) return 1;
-
-        long sum = 0;
         int maxi = 0;
-        for(int  i :milestones)
-        {
-            maxi = Math.max(maxi,i);
-            sum+=i;
+        int length = milestones.length;
+        long sum = 0; 
+
+        for(int i=0;i<length;i++){
+            maxi = Math.max(maxi,milestones[i]);
+            sum+= (long)milestones[i];
         }
 
-        long rest = sum-maxi;
-        if(rest<maxi) return 2*rest+1;
-        else return sum;
+        sum-=maxi;
+        if(maxi>sum)
+            return 2l*sum+1;
+        else
+            return (long)(sum+maxi);
 
     }
 }
 
-/**
-    
-    last = 1  
-    count = 7
-
-    another way: if we sort array in ascending order : 1,2,5
-                if 1 occurs one time , 2 will occur 2 time the next will occur 3 times
-                [1,2,4,8]
-                [2,3,5,9]
-                [2,3,5]
-                [0,0,0]
-                mask= 1
-                count = 5
-                [0,1,3]
- */
+/*
+2
+1
+if(max>sum of rest)
+    ans = sum of rest*2
+else
+    ans = total sum;
+*/
