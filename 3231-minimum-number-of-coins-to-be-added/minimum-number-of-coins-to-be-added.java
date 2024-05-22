f6b1411710ba76aac095c7b9f8dad297b6c46d5e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-22 14:41:19 +0530

    Time: 26 ms (11.11%) | Memory: 62.3 MB (13.19%) - LeetSync

--- 3231-minimum-number-of-coins-to-be-added/minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/minimum-number-of-coins-to-be-added.java
@@ -1,26 +1,25 @@
 class Solution {
- 
     public int minimumAddedCoins(int[] coins, int target) {
         Arrays.sort(coins);
-        int j = 0;
-        long prefixSum = 0l;
+        long prefixSum = 0;
+        int j =0;
         int additionalCoins = 0;
-
-        for(int i=1;i<=target;i++)
-        {
-            while(j<coins.length && coins[j]<=i)
-            {
-                prefixSum+=(long)coins[j];
+        for(int i=1;i<=target;i++){
+            while(j<coins.length && coins[j]<=prefixSum+1){
+                prefixSum+=coins[j];
                 j++;
             }
-
-            if(i>prefixSum)
-            {
-                prefixSum+=(long)i;
+            if(i>prefixSum){
+                prefixSum+=i;
                 additionalCoins++;
             }
         }
-        
         return additionalCoins;
     }
 }
+
+/*
+    WHY DOES IT WORK? 
+    If you are able to form prefix sum  = j ;
+    then  you can form all numbers from 1 to j till you encounter number > prefixSum+1
+ */
