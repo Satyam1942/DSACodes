da28f3976dfcc1f23e4859046070222b0f977a50
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-23 23:41:50 +0530

    Time: 4 ms (39.29%) | Memory: 45 MB (13.57%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,25 +1,28 @@
 class Solution {
-    double rec(int poured , int query_row, int query_glass, Double [][] dp)
-    {
-        if(query_row < 0 || query_glass<0 || query_glass>query_row) return 0.0;
+    double filled(int poured, int queryRow, int queryGlass, Double cache[][]){
+        if(queryRow<0 || queryGlass<0 || queryGlass>queryRow)
+            return 0.0;
+        if(queryRow==0 && queryGlass==0)
+            return (double)poured;
+        if(cache[queryRow][queryGlass]!=null)
+            return cache[queryRow][queryGlass];
+        
+        double upLeft = filled(poured,queryRow-1,queryGlass-1,cache);
+        double upRight = filled(poured,queryRow-1,queryGlass,cache);
 
-        if(query_row ==0 && query_glass==0) return dp[query_row][query_glass] = (double) poured;
-       
-        if(dp[query_row][query_glass]!=null) return dp[query_row][query_glass];
-
-        double upLeft = rec(poured,query_row-1,query_glass-1,dp);
-        double upRight = rec(poured, query_row-1,query_glass,dp);
-
-        double leftRes = (upLeft-1)/2.0 ;
+        double leftRes = (upLeft-1)/2.0;
         double rightRes = (upRight-1)/2.0;
 
-          if(leftRes<0) leftRes = 0.0;
-          if(rightRes<0) rightRes = 0.0;
+        if(leftRes<0)
+            leftRes = 0.0;
+        if(rightRes<0)
+            rightRes = 0.0;
 
-          return dp[query_row][query_glass] = leftRes+ rightRes;
+        return cache[queryRow][queryGlass] = leftRes+rightRes;           
     }
     public double champagneTower(int poured, int query_row, int query_glass) {
-        Double dp[][] = new Double[100][100];
-       return Math.min(1.0 ,rec(poured, query_row,query_glass,dp));
+        Double cache[][] = new Double[100][100];
+        double ans = filled(poured,query_row,query_glass,cache);
+        return Math.min(1,ans);
     }
 }
