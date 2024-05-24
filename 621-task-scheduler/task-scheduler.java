9bc3af8c249d92d289bcfd03ea8acbe8388aa10b
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-24 22:42:20 +0530

    Time: 45 ms (11.33%) | Memory: 45.1 MB (91.22%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,68 +1,68 @@
-class Triplet {
+class Pair implements Comparator<Pair>{
+    char task;
+    int count;
+    Pair(){
+
+    }
+    Pair(char task, int count){
+        this.task = task;
+        this.count = count;
+    }
+
+    @Override
+    public int compare(Pair a, Pair b){
+        return b.count-a.count;
+    }
+}
+
+class Triplet{
     char task;
     int count;
     int time;
-
-    Triplet(char task, int time, int count) {
+    Triplet(char task, int count, int time){
         this.task = task;
+        this.count =count;
         this.time = time;
-        this.count = count;
     }
 }
 
-class Pair {
-    char task;
-    int count;
-
-    Pair(char task, int count) {
-        this.task = task;
-        this.count = count;
-    }
-}
-
-class CustomComparator implements Comparator<Pair> {
-    public int compare(Pair a, Pair b) {
-        return b.count - a.count;
-    }
-}
 
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new CustomComparator());
-        Queue<Triplet> q = new LinkedList<>();
         int freq[] = new int[26];
-
-        for (int i = 0; i < tasks.length; i++) {
-            char curTask = tasks[i];
-            freq[(int) curTask - 65]++;
+        int noOfTasks = tasks.length;
+        for(int i=0;i<noOfTasks;i++){
+            char token = tasks[i];
+            freq[(int)token-65]++;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] == 0)
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new Pair());
+        Queue<Triplet> q = new LinkedList<>();
+
+        for(int i=0;i<26;i++){
+            if(freq[i]==0)
                 continue;
-
-            char curTask = (char) (i + 65);
-            pq.add(new Pair(curTask, freq[i]));
+            char token = (char)(i+65);
+            pq.add(new Pair(token,freq[i]));
         }
-        
-        int time = 0;
-        while (!q.isEmpty() || !pq.isEmpty()) {
 
-            while (!q.isEmpty() && q.peek().time < time) {
-                char task = q.peek().task;
+        int time = 0;
+        while(!pq.isEmpty() || !q.isEmpty()){
+            while(!q.isEmpty() && q.peek().time<time){
+                char task  = q.peek().task;
                 int count = q.peek().count;
                 q.poll();
-                pq.add(new Pair(task, count));
+                pq.add(new Pair(task,count));
             }
 
-            if (!pq.isEmpty()) {
+            if(!pq.isEmpty()){
                 char task = pq.peek().task;
-                int curFreq = pq.peek().count;
+                int  count = pq.peek().count;
                 pq.poll();
-
-                if (curFreq != 1)
-                    q.add(new Triplet(task,time+n, curFreq - 1));
+                if(count>1){
+                    int newTime = n+time;
+                    q.add(new Triplet(task,count-1,newTime));
+                }
             }
 
             time++;
