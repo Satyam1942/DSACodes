4d85842529a8def0a933edb55a59a9e6a555b981
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-25 08:14:33 +0530

    Time: 26 ms (96.05%) | Memory: 64.2 MB (12.28%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,50 +1,30 @@
-class Pair {
-    int index, val;
-
-    Pair(int val, int index) {
-        this.val = val;
-        this.index = index;
-    }
-}
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
         int length = nums.length;
-        int maxWindow[] = new int[length - k + 1];
+        int maxSlidingWindow[] = new int[length-k+1];
 
-        ArrayDeque<Pair> deq = new ArrayDeque<>();
-
-        int left = 0, right = left + k - 1;
-        while (right < length) {
-            if (left == 0) {
-                for (int i = left; i <= right; i++) {
-                    while (!deq.isEmpty() && nums[i] > deq.peekFirst().val)
-                        deq.removeFirst();
-                    deq.addFirst(new Pair(nums[i], i));
-                }
+        for(int i=0;i<k;i++){
+            while(!deque.isEmpty() && nums[deque.peekLast()]<nums[i]){
+                deque.pollLast();
             }
-
-            // selecting max from deque
-            maxWindow[left] = deq.peekLast().val;
-
-            left++;
-            right++;
-
-            // removing from deque
-            while (!deq.isEmpty() && deq.peekLast().index < left)
-                deq.removeLast();
-
-            // add into deque
-            if (right < length) {
-                while (!deq.isEmpty() && nums[right] > deq.peekFirst().val)
-                    deq.removeFirst();
-                deq.addFirst(new Pair(nums[right], right));
-            }
-
+            deque.addLast(i);
         }
 
-        return maxWindow;
 
+        for(int i=k;i<length;i++){
+            maxSlidingWindow[i-k] = nums[deque.peekFirst()];
+            if(deque.peekFirst()<=i-k)
+                deque.removeFirst();
+                
+            while(!deque.isEmpty() && nums[deque.peekLast()]<nums[i]){
+                deque.pollLast();
+            }
+            deque.addLast(i);
+        
+        }
+        maxSlidingWindow[length-k] = nums[deque.peekFirst()];
+        
+        return maxSlidingWindow;
     }
 }
