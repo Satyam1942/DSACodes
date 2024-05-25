b7f5d72129466c76dba88b425915f0fbdf230dbf
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-25 09:41:38 +0530

    Time: 17 ms (26.13%) | Memory: 56.9 MB (5.40%) - LeetSync

--- 754-cracking-the-safe/cracking-the-safe.java
+++ 754-cracking-the-safe/cracking-the-safe.java
@@ -1,30 +1,39 @@
 class Solution {
-   String ans = "";
-    boolean dfs(String stringStream, HashSet<String> vis, int k,int n )
-    {  
-        String lastNCharacters = stringStream.substring(stringStream.length()-n);
-        if(vis.contains(lastNCharacters)) return false ;
-        vis.add(lastNCharacters);
-        if(vis.size() == Math.pow(k,n)) return true;
+    String ans = "";
+    boolean dfs(String s, int n, int k, HashSet<String> vis){
+        String lastNCharacter = s.substring(s.length()-n,s.length());
+     
+        //path has added redundancy so dont take it
+        if(vis.contains(lastNCharacter))
+            return false;
 
-        for(int i=0;i<k;i++)
-        {
-           boolean temp = dfs(stringStream+ String.valueOf(i),vis,k,n);
-            if(temp){ans = (char)(i+48)+ans; return true;}
-        }  
+        vis.add(lastNCharacter);
+        if(vis.size()==(int)Math.pow(k,n))
+            return true;
 
-        vis.remove(lastNCharacters);
+        for(int i=0;i<k;i++){
+            String nstr =  s+String.valueOf(i);
+          
+            boolean temp = dfs(nstr,n,k,vis);
+            if(temp){
+                ans = String.valueOf(i)+ans;
+                return true;
+            }
+        }
+
+        vis.remove(lastNCharacter);
         return false;
     }
-
     public String crackSafe(int n, int k) {
+        StringBuilder init = new StringBuilder();
+        for(int i=0;i<n;i++)
+            init.append("0");
         HashSet<String> vis = new HashSet<>();
-        String str  = "";
-        
-        for(int i=0;i<n;i++) str+="0";
 
-        dfs(str,vis,k,n);
-        for(int i=0;i<n;i++) ans="0"+ans;
+        dfs(init.toString(),n,k,vis);
+        for(int i=0;i<n;i++)
+            ans="0"+ans;
+            
         return ans;
     }
 }
