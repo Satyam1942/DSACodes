33b86e272d3859b09eca4a789842ae6dfb2fb57d
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-26 11:21:35 +0530

    Time: 55 ms (21.94%) | Memory: 55.1 MB (19.83%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.java
+++ 2808-painting-the-walls/painting-the-walls.java
@@ -1,20 +1,33 @@
 class Solution {
-long rec(int index ,int wallsLeft, int cost[], int time[],Long dp[][])
-    {
-        if(wallsLeft<=0) return 0;
-        if(index==cost.length) return (long)(1e15);
-        
-    if(dp[index][wallsLeft] !=null) return dp[index][wallsLeft];
+    int costOfPainting(int index, int walls, int cost[], int time[], Integer cache[][] ) {
+        if(walls<=0)
+            return 0;
+        if(index==cost.length)
+            return Integer.MAX_VALUE;
 
-      long freePainter = rec(index+1,wallsLeft,cost,time, dp);
-      long paidPainter = (long)cost[index]+ rec(index+1,wallsLeft-time[index]-1,cost,time,dp);
+        if(cache[index][walls]!=null)
+            return cache[index][walls];
 
-      return dp[index][wallsLeft]  = Math.min(freePainter,paidPainter);
+        int notPaint = costOfPainting(index+1,walls,cost,time,cache);
+        int paint =  costOfPainting(index+1,walls-1-time[index],cost,time,cache);
+        if(paint!=Integer.MAX_VALUE)
+            paint+=cost[index];
+        return cache[index][walls] = Math.min(paint,notPaint);
     }
 
     public int paintWalls(int[] cost, int[] time) {
-        int  length = cost.length;
-        Long dp[][] = new Long[length][length+1];
-        return (int)rec(0,cost.length,cost,time,dp);
+        Integer cache[][] = new Integer[cost.length][cost.length+1];
+        return costOfPainting(0,cost.length,cost,time,cache);
     }
 }
+
+/*
+ * if(walls<=0)
+ * return 0;
+ * if(index==cost.length)
+ * return Integer.MAX_VALUE;
+ * int notPaint = costOfPainting(index+1,walls,cost,time);
+ * int paint = cost[index] + costOfPainting(index+1,walls-1-time[index],cost,
+ * time);
+ * return min(paint,notPaint);
+ */
