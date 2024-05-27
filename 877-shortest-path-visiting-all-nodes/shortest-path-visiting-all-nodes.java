4af30c1d3de37c837bd8de3c6e750dcfbf1b88f1
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-27 17:08:56 +0530

    Time: 14 ms (44.19%) | Memory: 45.4 MB (35.35%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
@@ -1,105 +1,46 @@
-
 class Solution {
     public int shortestPathLength(int[][] graph) {
+        int numberOfNodes = graph.length;
+        int rows = (1<<numberOfNodes);
+        int cols = numberOfNodes;
+        int dist[][] = new int[rows][cols];
+
+        for(int i=0;i<rows;i++)
+            Arrays.fill(dist[i],(int)(1e9));
         
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-        
-        int n = graph.length;
-        
-        // Adjency list of graph
-        
-        for(int i=0;i<n;i++){
-            if(!hm.containsKey(i)){
-                hm.put(i, new ArrayList<Integer>());
-            }
-            
-            int m = graph[i].length;
-            for(int j=0;j<m;j++){
-                hm.get(i).add(graph[i][j]);
-            }
-            
-        }
-        
-        //dist 2d array
-        //row: bitmask -> visited node set bits are 1
-        //column: leading node
-        
-        int row = (int)Math.pow(2,n);
-        int col = n;
-        int dist[][] = new int[row][col];
-        
-        for(int i=0;i<row;i++){
-            Arrays.fill(dist[i], -1);
-        }
-        
-        
-        //Queue: [{leading node 1, mask},{leading node 2, mask} ... ]
-        LinkedList<int[]> q = new LinkedList<>();
-        
-        for(int i=0;i<n;i++){
-            
+        Queue<int[]>q = new LinkedList<>();
+        for(int i=0;i<numberOfNodes;i++){
             int lead = i;
-            int mask = setbit(0,i);
-            
-            q.add(new int[]{lead, mask});
+            int mask = 0^(1<<i);
+            q.add(new int[]{lead,mask});
             dist[mask][lead] = 0;
         }
-        
-        
-        // Applying simulatneous BFS
-        while(q.size()>0){
-            
-            
+
+        while(!q.isEmpty()){
             int size = q.size();
-            for(int i=0;i<size;i++){
-                
-                int[] path = q.remove();
-                
-                int lead = path[0];
-                int mask = path[1];
-                
-                if(mask == row-1){   //all nodes visited
-                    return dist[mask][lead];
-                }
-                
-                // iterate over neighbours of lead
-                if(hm.containsKey(lead)){
-                    
-                    for(int child: hm.get(lead)){
-                    
-                        int newlead = child;
-                        int newmask = setbit(mask, newlead);
+            while(size-->0){
+                int curNode = q.peek()[0];
+                int routeMask = q.peek()[1];
+                q.poll();   
+                if(routeMask==rows-1)
+                    return dist[routeMask][curNode];
 
-                        // avoid cycle: intelligent bfs : checking if this set is already visited 
-                        // set : lead, mask(visited nodes)
-                        if(dist[newmask][newlead]!=-1){
-                            continue;
-                        }
-
-                        dist[newmask][newlead] = dist[mask][lead]+1;
-                        q.add(new int[]{newlead, newmask});
-
+                for(int i=0;i<graph[curNode].length;i++){
+                    int adjNode  = graph[curNode][i];
+                    int adjRouteMask = routeMask|(1<<adjNode);
+                   
+                    if(dist[adjRouteMask][adjNode]>dist[routeMask][curNode]+1){
+                        dist[adjRouteMask][adjNode] = dist[routeMask][curNode]+1;
+                        q.add(new int[]{adjNode,adjRouteMask});
                     }
-
                 }
-                
             }
-            
         }
-        
-        
-        return 1221; //magic - LOL
-        
-        
-        
+
+        return  -1;
+
     }
-    
-    
-    private int setbit(int mask, int i){
-        return mask|(1<<i);
-    }
-    
-    
-    
-    
 }
+/*
+
+*/
