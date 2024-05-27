929b449cc88759edfc7ba77397b639cd8d9c741e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-27 13:10:39 +0530

    Time: 6 ms (87.72%) | Memory: 171.9 MB (70.18%) - LeetSync

--- 3290-number-of-subarrays-that-match-a-pattern-ii/number-of-subarrays-that-match-a-pattern-ii.java
+++ 3290-number-of-subarrays-that-match-a-pattern-ii/number-of-subarrays-that-match-a-pattern-ii.java
@@ -1,85 +1,71 @@
 class Solution {
-    String stringifyPattern(int arr[]){
-        StringBuilder sb = new StringBuilder();
-        for(int i =0;i<arr.length;i++){
-            if(arr[i]==-1){
-                sb.append(2);
-            }else
-                sb.append(arr[i]);
+    int[] modifyNum(int [] nums){
+        int length = nums.length;
+        int modifiedNum[] = new int[length-1];
+
+        for(int i=0;i<length-1;i++){
+            if(nums[i]<nums[i+1])
+                modifiedNum[i] = 1;
+            else if(nums[i]==nums[i+1])
+                modifiedNum[i] = 0;
+            else
+                modifiedNum[i] = -1;
         }
-        return  sb.toString();
+        return modifiedNum;
     }
 
-     String stringifyNum(int arr[]){
-        StringBuilder sb = new StringBuilder();
-        for(int i =0;i<arr.length-1;i++){
-            if((long)arr[i]<(long)arr[i+1])
-                sb.append(1);
-            else if(arr[i]==arr[i+1])
-                sb.append(0);
-            else 
-                sb.append(2);
-        }
-        return  sb.toString();
-    }
-
-
-    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+    int kmpAlgo(int nums[] , int pattern[]){
         int n = nums.length;
         int m = pattern.length;
-        String needle =  stringifyPattern(pattern);
-        String haystack = stringifyNum(nums);
 
-        // System.out.println(haystack);
-        // System.out.println(needle);
-
-         // KNUTH - MORRIS - PRATT (KMP) ALGORITHM
-
-        if(m>n) return -1;
-        
-        //Building LPS array O(m)   worse case TC= O(2m)
-        
         int lps[] = new int[m];
-        int i=1;
-        int prevLPS =0;
+        lps[0]= 0;
+        int prevLps = 0;
+        int i = 1;
 
         while(i<m){
-            if(needle.charAt(i)==needle.charAt(prevLPS)){
-                lps[i]= prevLPS+1;
-                prevLPS++;
+            if(pattern[prevLps]==pattern[i]){
+                lps[i] = prevLps +1;
+                prevLps++;
                 i++;
+            }else{
+                if(prevLps==0){
+                    lps[i] = 0;
+                    i++;
+                }else{
+                    prevLps  = lps[prevLps-1];
+                }
             }
-            else if(prevLPS==0) {
-                lps[i]=0; 
+        }
+         
+        i=0;
+        int j=0;
+
+        int count = 0;
+        while(i<n){
+            if(nums[i]==pattern[j]){
                 i++;
+                j++;
             }
-            else  
-                prevLPS = lps[prevLPS-1];
+            else{
+                if(j==0){
+                    i++;
+                }else{
+                    j = lps[j-1];
+                }
+            }
+
+            if(j==m){
+                count++;
+                j=lps[j-1];
+            }
         }
 
+        return count;
+    }
 
-        //Iterating through haystack O(n)
-        int count = 0;
-         int right =0, left=0;
-         while(right<n-1){
-             if(haystack.charAt(right)== needle.charAt(left)){
-                 right++;
-                 left++;
-             }
-             else{
-                 if(left==0)  
-                    right++;
-                 else  
-                    left = lps[left-1];
-            }
-
-             if(left==m) {
-                count++;
-                left = lps[left-1];
-             }
-         }
-
-         return count;
-
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        int modifiedNum[] = modifyNum(nums);
+        return kmpAlgo(modifiedNum,pattern);
     }
 }
