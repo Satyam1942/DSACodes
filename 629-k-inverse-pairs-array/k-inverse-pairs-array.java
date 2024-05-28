7f1f28e13fe580f60a3e552cd04701695e18b2fc
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-28 14:45:19 +0530

    Time: 16 ms (74.68%) | Memory: 46.6 MB (71.65%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.java
@@ -1,36 +1,25 @@
 class Solution {
     public int kInversePairs(int n, int k) {
-        long mod = (long)(1e9+7);
+        int mod = (int)(1e9+7);
 
-        long prev[] = new long[k+1];
-        //base case
-        prev[0] = 1;
-        
-       for(int i=1;i<=n;i++){
-           
-            long cur[] = new long[k+1];
-            long total = 0; // sliding window using total variable
-            
+        int cache[][] = new int[n+1][k+1];
+        cache[1][0] = 1;
+
+        for(int i=2;i<=n;i++){
+            int sum = 0;
             for(int j=0;j<=k;j++){
-                if(j>=i) // remove from window 
-                    total= (mod + total-prev[j-i])%mod;
-            
-                total = (total + prev[j])%mod; // add to sliding window
-                cur[j] = total;
+                if(j<=i-1){
+                    sum = (sum+cache[i-1][j])%mod;
+                    cache[i][j] = sum;
+                }else{
+                    sum = (sum+cache[i-1][j])%mod;
+                    sum = (sum-cache[i-1][j-i]+mod)%mod;
+                    cache[i][j] = sum;
+                }
             }
-
-            prev = cur;
         }
         
-        return (int)prev[k];
+        return cache[n][k];
+
     }
 }
-
-/*
-HERE DP[n][k] represents no of arrays of size n with k reverse pairs
-We use sliding window to optimize from n*n*k to n*k
-This is only possible using the tabulation approach
-
-Further I have space optimised dp[n][k] using prev and cur cause for filling 
-current array we only need one array previous instead of all arrays from 0 to n
-*/
