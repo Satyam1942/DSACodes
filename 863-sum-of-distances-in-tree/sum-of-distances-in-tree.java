e37dfc4d7d99f75e671f29c5ad94471bef4ac948
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-28 23:11:48 +0530

    Time: 40 ms (69.61%) | Memory: 64.5 MB (76.11%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.java
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.java
@@ -1,61 +1,59 @@
 class Solution {
-    int postOrderTraversal(int curNode, int subNodes[] , int res[], int vis[] ,  List<List<Integer>> adjList ){
-       vis[curNode] =1;
-       int noOfSubNodes = 0;
-       int resSum = 0;
-        for(int i=0;i<adjList.get(curNode).size();i++){
-            int adjNode = adjList.get(curNode).get(i);
-            if(vis[adjNode]==0){
-            noOfSubNodes+=postOrderTraversal(adjNode, subNodes, res, vis,adjList);
-            resSum+=res[adjNode];
-            }
-        }
-
-        res[curNode] = resSum + noOfSubNodes;
-        subNodes[curNode] = noOfSubNodes+1;
-
-        return noOfSubNodes+1;
-    }
-
-    void preOrderTraversal(int curNode, int prevNode, int totalNodes, int subNodes[], int res[],int vis[] , List<List<Integer>> adjList){
+    public int getNodeCount(int curNode , int childCount[] , int vis[], List<List<Integer>> adjList){
         vis[curNode] = 1;
-        if(prevNode!=-1){
-            res[curNode] = res[prevNode] - subNodes[curNode] + (totalNodes-subNodes[curNode]);
-        }
+
+        int countOfChild = 1;
+        int distance = 0;
 
         for(int i=0;i<adjList.get(curNode).size();i++){
             int adjNode = adjList.get(curNode).get(i);
             if(vis[adjNode]==0){
-            preOrderTraversal(adjNode,curNode, totalNodes, subNodes, res, vis, adjList);
+                distance+= getNodeCount(adjNode,childCount, vis,adjList);
+                countOfChild+= childCount[adjNode];
             }
         }
+
+        childCount[curNode] = countOfChild;
+
+        return distance+countOfChild-1;
     }
 
+    public void getDistance(int curNode , int distanceHistory, int childCount[], int vis[] , int sumOfDistanceTree[], List<List<Integer>> adjList ){
+        vis[curNode] = 1;
+        for(int i=0;i<adjList.get(curNode).size();i++){
+            int adjNode = adjList.get(curNode).get(i);
+       
+            if(vis[adjNode]==0){
+                int newDistanceHistory = distanceHistory + childCount[curNode]-childCount[adjNode];
+                sumOfDistanceTree[adjNode] = sumOfDistanceTree[curNode]+newDistanceHistory-childCount[adjNode];
+                getDistance(adjNode,newDistanceHistory,childCount, vis ,sumOfDistanceTree,adjList);
+            }
+        }
+
+    }
 
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        int res[] = new int[n];
-        int subNodes[] = new int[n];
+        int m = edges.length;
         List<List<Integer>> adjList = new ArrayList<>();
-
-        int vis[] = new int[n];
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             adjList.add(new ArrayList<>());
-        
-        for(int i=0;i<edges.length;i++){
+        }    
+
+        for(int i=0;i<m;i++){
             adjList.get(edges[i][0]).add(edges[i][1]);
             adjList.get(edges[i][1]).add(edges[i][0]);
         }
 
-        postOrderTraversal(0,subNodes,res,vis,adjList);
+        int childNodeCount[] = new int[n];
+        int sumOfDistanceTree[] = new int[n];
+        int vis[] = new int[n];
+        sumOfDistanceTree[0] = getNodeCount(0,childNodeCount,vis,adjList);
+     
         Arrays.fill(vis,0);
-        preOrderTraversal(0,-1,n,subNodes,res,vis,adjList);
 
-        return res;
+        getDistance(0,0,childNodeCount,vis, sumOfDistanceTree,adjList);
+
+        return sumOfDistanceTree;
+
     }
 }
-
-/*          
-            0   1   2   3   4   5 
-  count=    6   1   4   1   1   1
-  res =     8   0   3   0   0   0
-*/
