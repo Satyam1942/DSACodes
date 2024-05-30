6cb5bdd54435c4923ee03006bcf747113f3f839d
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-05-30 10:59:03 +0530

    Time: 2 ms (77.44%) | Memory: 41.4 MB (19.49%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,3 +1,11 @@
+class Pair{
+    int index, freq;
+    Pair(int index, int freq){
+        this.index = index;
+        this.freq = freq;
+    }
+}
+
 class Solution {
     public int countTriplets(int[] nums) {
         int length = nums.length;
@@ -8,14 +16,22 @@ class Solution {
             prefixXor[i] = xor;
         }
 
+        HashMap<Integer,Pair> seenXor = new HashMap<>();
+        seenXor.put(0,new Pair(-1,1));
+        int cache[] = new int[length+1];
+
         int count = 0;
-
-        for (int i = 0; i < length; i++) {
-            for (int k = i + 1; k < length; k++) {
-
-                int xorItoK = (i == 0) ? prefixXor[k] : prefixXor[k] ^ prefixXor[i - 1];
-                if (xorItoK == 0)
-                    count+=(k-i);
+        for(int i= 0;i<length;i++){
+            int curXor = prefixXor[i];
+            if(!seenXor.containsKey(curXor))
+                seenXor.put(curXor,new Pair(i,1));
+            else{
+                int index = seenXor.get(curXor).index;
+                int freq = seenXor.get(curXor).freq;
+                int res = (freq-1)*(i-index)+ (i-index-1)+cache[index+1];
+                cache[i+1] = res;
+                count+= res;
+                seenXor.put(curXor,new Pair(i,freq+1));
             }
         }
 
