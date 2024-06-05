52452351de23bf3b1471409d64741d1c43dc49d4
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-05 11:42:59 +0530

    Time: 20 ms (74.57%) | Memory: 43.8 MB (30.90%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -1,57 +1,76 @@
 class Solution {
-    long gcd(long a,long b){
-        long ans = Math.min(a,b);
-        long num = Math.max(a,b);
-        while(ans>0){
-            long remainder = num%ans;
-            num = ans;
-            ans = remainder;
+    boolean isPossible(long val, int k, List<Long> lcmList ){
+        long rank = 0;
+        for(long lcm: lcmList){
+            rank+= val/lcm;
         }
-
-        return num;
+        return rank>=k;
+    }
+    
+    long getGcd(long a, long b){
+        long gcd = Math.min(a,b);
+        long temp = Math.max(a,b);
+        b = temp;
+        a = gcd;
+        while(b%a>0){
+            long r = b%a;
+            b = a;
+            a = r;
+            gcd = a;
+        }
+        return gcd;
     }
 
-    long getLCM(long a,long b){
-        return a*b/gcd(a,b);
-    }
-
-    boolean isPossible(long mid, long k, List<Long> lcm){
-   
-        long count = 0;
-        for(long i:lcm){
-            count+= mid/i;
-        }
-        return count>=k;
+    long getLcm(long a, long b){
+        return (a*b)/getGcd(a,b);
     }
 
     public long findKthSmallest(int[] coins, int k) {
         Arrays.sort(coins);
+        List<Long> lcmList = new ArrayList<>();
         int length = coins.length;
 
-        List<Long> lcm = new ArrayList<>();
         for(int i=1;i<(1<<length);i++){
-            long l = 1;
-            for(int j=0;j<length;j++){
-                if((i&(1<<j)) >0){
-                    l = getLCM(l,(long)coins[j]);
+            long lcm = 0;
+            int count = 0;
+            for(int bit = 0; bit<31;bit++){
+                if((i&(1<<bit))>0){
+                    count++;
+                    if(lcm==0)
+                        lcm = (long)coins[bit];
+                    else
+                        lcm = getLcm(lcm,(long)coins[bit]);
                 }
             }
-            lcm.add(l * (Integer.bitCount(i) % 2 == 1 ? 1 : -1));
+            lcmList.add(lcm*((count%2==0)?-1:1));
+        }
+   
+
+        long left = coins[0];
+        long right = (long)coins[0]*(long)k;
+        long val = right;
+
+        while(left<=right){
+            long mid = left+(right-left)/2;
+            if(isPossible(mid,k,lcmList)){
+                right = mid-1;
+                val = mid;
+            }else
+                left = mid+1;
         }
 
-        long lowerBound  = coins[0];
-        long upperBound =  (long)k*(long)coins[0];
-        long ans = upperBound;
-        while(lowerBound<=upperBound){
-            long mid = lowerBound+(upperBound-lowerBound)/2l;
-            if(isPossible(mid,k,lcm)){
-                ans = mid;
-                upperBound = mid-1;
-            }else{
-                lowerBound = mid+1;
-            }
-        }
-        return ans;
-        
+        return val;
     }
 }
+
+/*
+    3=> 3,6,9
+    6=> 6,12
+    9=> 9,18
+
+    min = 2
+    max = 14
+    coins = 2,5
+    find lcm of numbers with which it is divisible
+    2,5,10
+ */
