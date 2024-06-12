5a9f8626b39802bd9d7d9b8f0d4f10dd623a9672
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-12 07:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (77.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -6,41 +6,32 @@ class Solution {
     }
 
     public void sortColors(int[] nums) {
-        int left = 0;
-        int right = 1;
         int length = nums.length;
+        int left = 0;
+        int mid = 0;
+        int right = length-1;
 
-        while(right<=length){
-            while(left<length && nums[left]==0)
-                left++;
-            if(left==length)
-                break;
-            
-            right = Math.max(right,left+1);
-            if(right<length && nums[right]==0){
-                swap(nums,left,right);
-            }
-            right++;
-        }
-    
-        right = left+1;
-        while(right<=length){
-            while(left<length && nums[left]==1){
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums,left,mid);
                 left++;
             }
-            if(left==length)
-                break;
-            right = Math.max(right,left+1);
-            if(right<length && nums[right]==1){
-                swap(nums,left,right);
+            if(nums[mid]==2){
+                swap(nums,right,mid);
+                right--;
             }
-            right++;
+            if(nums[mid]==1)
+                mid++;
+            if(mid<left)
+                mid=left;
         }
 
     }
 }
-/*
-                   r
-    0, 0, 2, 1, 1, 2
-          l
+/*          l
+            m   r    
+   0    0   1   1   2   2
+   
+    swap  0 of mid with left 
+    swap  2 of mid with right
  */
