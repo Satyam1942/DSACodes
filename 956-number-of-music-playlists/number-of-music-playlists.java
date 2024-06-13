99780ade070a089278cf4eeb239bae38b419c189
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-13 08:13:37 +0530

    Time: 1 ms (98.64%) | Memory: 41.9 MB (57.82%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.java
+++ 956-number-of-music-playlists/number-of-music-playlists.java
@@ -1,32 +1,46 @@
 class Solution {
     long mod = (long)(1e9+7);
-    long calculateMusicPlaylist(int oldSongs, int goal, int n, int k, Long cache[][]) {
-        if (goal == 0) {
-            if (oldSongs == n)
-                return 1;
+    long numberOfPlaylists(int oldSongs, int goal, int k, int totalSongs, Long cache[][]){
+        if(goal==0){
+            if(oldSongs==totalSongs)
+                return 1l;
             else
-                return 0;
+                return 0l;
         }
 
         if(cache[oldSongs][goal]!=null)
             return cache[oldSongs][goal];
 
-        long pickOldSong = 0;
-        long pickNewSong = 0;
+        long newSongs = totalSongs -oldSongs;
+        long noOfOldSongsCanPlay = oldSongs-k;
 
-        if(oldSongs>=k)
-            pickOldSong =  (oldSongs-k)*calculateMusicPlaylist(oldSongs, goal - 1, n, k,cache);
-        if(oldSongs<n)
-            pickNewSong =  (n-oldSongs)*calculateMusicPlaylist(oldSongs+1, goal - 1, n, k,cache);
+        long chooseNewSong =0;
+        long chooseOldSong = 0;
+       
+        if(newSongs>0) 
+            chooseNewSong = (newSongs*numberOfPlaylists(oldSongs+1,goal-1,k,totalSongs,cache))%mod;
         
-        return cache[oldSongs][goal] = (pickOldSong%mod+pickNewSong%mod)%mod;
-    }
+        if(noOfOldSongsCanPlay>0)
+            chooseOldSong = ((noOfOldSongsCanPlay)*numberOfPlaylists(oldSongs,goal-1, k, totalSongs,cache))%mod;
 
+        return cache[oldSongs][goal] = (chooseNewSong+chooseOldSong)%mod;
+    }
     public int numMusicPlaylists(int n, int goal, int k) {
         Long cache[][] = new Long[n+1][goal+1];
-
-        int noOfPlaylists =  (int)calculateMusicPlaylist(0,goal,n,k,cache);
-        
-        return noOfPlaylists;
+        return (int)numberOfPlaylists(0,goal,k,n,cache);
     }
 }
+/*
+    1,2
+    1,2,1
+    2,1,2
+
+    int goal,
+    either play a new song if available 
+    oldSongs = totalSongs - newSongs
+    (total-oldSongs)*f(oldSongs+1,goal-1,k)
+    play an old 
+    if(oldSongs>k)
+        (oldSongs-k)*f(oldSongs,goal-1,k)
+    123
+ */
