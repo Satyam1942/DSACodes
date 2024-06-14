798c36804b7b5aa4769434a5d10701daf5b74f26
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-14 15:23:46 +0530

    Time: 4 ms (16.62%) | Memory: 45 MB (6.27%) - LeetSync

--- 685-redundant-connection-ii/redundant-connection-ii.java
+++ 685-redundant-connection-ii/redundant-connection-ii.java
@@ -1,132 +1,98 @@
-class DisjointSet
-{
+class DisjointSet {
     List<Integer> parent;
     List<Integer> size;
-    DisjointSet(int n)
-    {
-        parent=  new ArrayList<>();
+
+    DisjointSet(int n) {
+        parent = new ArrayList<>();
         size = new ArrayList<>();
-        for(int i=0;i<n;i++) {parent.add(i); size.add(1);}
-    }
-
-    int findUltimateParent(int node)
-    {
-        if(parent.get(node)==node) return node;
-        int ultmParent = findUltimateParent(parent.get(node));
-        parent.set(node,ultmParent);
-        return ultmParent;
-    }
-
-    void unionBySize(int u, int v)
-    {
-        int ultimU = findUltimateParent(u);
-        int ultimV = findUltimateParent(v);
-
-        if(ultimU==ultimV) return;
-        if(size.get(ultimU)>size.get(ultimV))
-        {
-            parent.set(ultimV,ultimU);
-            size.set(ultimU, size.get(ultimU)+size.get(ultimV));
+        for (int i = 0; i < n; i++) {
+            parent.add(i);
+            size.add(1);
         }
-        else
-        {
-            parent.set(ultimU,ultimV);
-            size.set(ultimV, size.get(ultimU)+size.get(ultimV));
+    }
+
+    int findUltimateParent(int u) {
+        if (u == parent.get(u))
+            return u;
+        int ultimPar = findUltimateParent(parent.get(u));
+        parent.set(u, ultimPar);
+        return ultimPar;
+    }
+
+    void unionBySize(int u, int v) {
+        int ultimParU = findUltimateParent(u);
+        int ultimParV = findUltimateParent(v);
+
+        if (ultimParU == ultimParV)
+            return;
+
+        if (size.get(ultimParU) >= size.get(ultimParV)) {
+            parent.set(ultimParV, ultimParU);
+            size.set(ultimParU, size.get(ultimParU) + size.get(ultimParV));
+        } else {
+            parent.set(ultimParU, ultimParV);
+            size.set(ultimParV, size.get(ultimParU) + size.get(ultimParV));
         }
     }
 }
 
-
 class Solution {
-    void fillIndegree(int [][] edges , int indegree[])
-    {
-        for(int i=0;i<edges.length;i++)
-        {
-            indegree[edges[i][1]]++;
+
+    boolean isValid(int indegree[], int edges[][], int index) {
+        int noOfNodes = indegree.length - 1;
+        int freq[] = new int[2];
+        
+        for (int i = 1; i <= noOfNodes; i++) {
+            if (indegree[i] > 1)
+                return false;
+            freq[indegree[i]]++;
         }
+   
+        if(freq[0]!=1)
+            return false;
+
+        DisjointSet ds = new DisjointSet(noOfNodes + 1);
+
+        for (int i = 0; i < edges.length; i++) {
+            if (index == i)
+                continue;
+            int u = ds.findUltimateParent(edges[i][0]);
+            int v = ds.findUltimateParent(edges[i][1]);
+            if (u == v)
+                continue;
+            else {
+                ds.unionBySize(u, v);
+            }
+        }
+
+
+        for(int i=1;i<noOfNodes;i++){
+            int u = ds.findUltimateParent(i);
+            int v = ds.findUltimateParent(i+1);
+            if(u!=v)
+                return false;
+
+        }
+
+        return true;
     }
 
-
     public int[] findRedundantDirectedConnection(int[][] edges) {
-        int numberOfNodes = edges.length;
-        int indegree[] = new int[numberOfNodes+1];
+        int noOfNodes = edges.length;
+        int noOfEdges = edges.length;
 
-        fillIndegree(edges,indegree);
-
-        boolean isType2 = false;
-        for(int i=0;i<=numberOfNodes;i++)
-        {
-            if(indegree[i]==2) {isType2 = true; break;}
+        int indegree[] = new int[noOfNodes + 1];
+        for (int i = 0; i < noOfEdges; i++) {
+            indegree[edges[i][1]]++;
         }
 
-        DisjointSet ds = new DisjointSet(numberOfNodes+1);
-
-      
-        if(!isType2)
-        {
-        for(int i=0;i<edges.length;i++)
-        {
-            if(ds.findUltimateParent(edges[i][0])!=ds.findUltimateParent(edges[i][1]))
-            {
-                ds.unionBySize(edges[i][0],edges[i][1]);
-            }
-            else 
-            {
-                int ans[] = new int[2];
-                ans[0] = edges[i][0];
-                ans[1] = edges[i][1];
-                return ans;
-            }
-        }
-        }
-        else 
-        {
-       
-            int cycleIndex= -1 ;
-        for(int i=0;i<indegree.length;i++)
-        {
-            if(indegree[i]==2) {cycleIndex = i; break;}
-        }
-
-        int edge1 = -1, edge2 = -1;
-        for(int i=0;i<edges.length;i++)
-        {
-            if(edges[i][1]==cycleIndex)
-            {
-                if(edge1==-1) edge1 = i;
-                else edge2 = i;
-            }
-        }   
-
-        boolean hasCycle = false;
-         for(int i=0;i<edges.length;i++)
-        {
-            if(i==edge2) continue;
-
-            if(ds.findUltimateParent(edges[i][0])!=ds.findUltimateParent(edges[i][1]))
-            {
-                ds.unionBySize(edges[i][0],edges[i][1]);
-            }
-            else 
-            {
-                hasCycle = true;
-                break;
-            }
-        }
-
-        if(hasCycle) return edges[edge1];
-        else return edges[edge2];
+        for (int i = noOfEdges - 1; i >= 0; i--) {
+            indegree[edges[i][1]]--;
+            if (isValid(indegree,edges,i))
+                return edges[i];
+            indegree[edges[i][1]]++;
         }
 
         return new int[2];
+    }
 }
-}
-/*
-    
-    case 1:
-    all indegree = 1
-    case 2: 1 indegree= 0 (root) and 1 node indegree = 2
-    case2.1 : Has cycle , 2.2 has no cycle
-    Using DSU remove each edge and check if graph is connected
-
- */
