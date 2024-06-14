f0b3737bd34d384ecac2ba8b98bba0dc58d4f70d
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-14 09:37:48 +0530

    Time: 1 ms (53.81%) | Memory: 41 MB (33.25%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,15 +1,4 @@
 class Solution {
-    int getScore(int left, int right, int nums[], Integer cache[][]){
-        if(left>right)
-            return 0;
-        if(cache[left][right]!=null)
-            return cache[left][right];
-
-        int pickLeft = nums[left]-getScore(left+1,right,nums,cache);
-        int pickRight = nums[right]-getScore(left,right-1,nums,cache);
-
-        return cache[left][right] = Math.max(pickLeft,pickRight);
-    }
     public boolean predictTheWinner(int[] nums) {
         int length  = nums.length;
         int cache[][] = new int[length+1][length+1];
