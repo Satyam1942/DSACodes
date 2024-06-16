763be6d00f69e91ab518515d8521bc4b97162c3a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-16 07:28:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (21.13%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long prefixSum = 0;
+        int count = 0;
+        int length = nums.length;
+    
+        for(int i=0;i<length;){
+            if(prefixSum>=n)
+                break;
+                
+            if(prefixSum+1<nums[i]){
+                count++;
+                prefixSum+= (prefixSum+1);
+            }else{
+                prefixSum+=nums[i];
+                i++;
+            }
+        }
+
+        while(prefixSum<n){
+            prefixSum+=(prefixSum+1);
+            count++;
+        }
+
+        return count;
+    }
+}
+/*
+    1, 5, 10
+    2,4 
+    2
+ */
