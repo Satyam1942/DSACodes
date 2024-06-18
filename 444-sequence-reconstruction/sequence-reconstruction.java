a130cd44f7f6aa8cfd3eb401c011fa9604c3b6e4
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-18 10:45:07 +0530

    Time: 39 ms (48.34%) | Memory: 55 MB (30.46%) - LeetSync

--- 444-sequence-reconstruction/sequence-reconstruction.java
+++ 444-sequence-reconstruction/sequence-reconstruction.java
@@ -1,65 +1,60 @@
 class Solution {
     public boolean sequenceReconstruction(int[] nums, List<List<Integer>> sequences) {
-        int length = nums.length;
-        List<List<Integer>> adjList = new ArrayList<>();
-        List<Integer> topoSort = new ArrayList<>();
-        int indegree[] = new int[length+1];
-        for(int i=0;i<=length;i++)
-        {
-            adjList.add(new ArrayList<>());
-        }
+        int numberOfNodes = nums.length;
+        List<HashSet<Integer>> adjList = new ArrayList<>();
+        List<Integer> toposort = new ArrayList<>();
 
-        for(int i=0;i<sequences.size();i++)
-        {
-            for(int j=1;j<sequences.get(i).size();j++)
-            {
-                adjList.get(sequences.get(i).get(j-1)).add(sequences.get(i).get(j));
-                indegree[sequences.get(i).get(j)]++;
+        for(int i=0;i<=numberOfNodes;i++)
+            adjList.add(new HashSet<>());
+
+        for(int i=0;i<sequences.size();i++){
+            for(int j=0;j<sequences.get(i).size()-1;j++){
+                int  u = sequences.get(i).get(j);
+                int  v = sequences.get(i).get(j+1);
+                adjList.get(u).add(v);
             }
-        }
+        } 
 
-        //Check if it hads unique toposort
+        int indegree[] = new int[numberOfNodes+1];
+        for(int i=1;i<=numberOfNodes;i++){
+            HashSet<Integer> set = adjList.get(i);
+            for(int node:set)
+                indegree[node]++;
+        }      
 
         Queue<Integer> q = new LinkedList<>();
-        int vis[] = new int[length+1];
-        
-        for(int i=1;i<=length;i++)
-        {
-            if(indegree[i]==0)
-            {
+        for(int i=1;i<=numberOfNodes;i++){
+            if(indegree[i]==0){
                 q.add(i);
-                vis[i]=1;
-                topoSort.add(i);
-            }
+                toposort.add(i);
+            }        
         }
-        
-        if(q.isEmpty()|| q.size()>1) return false;
 
-        while(!q.isEmpty())
-        {
-            int curNode=  q.poll();
-              int count = 0;
-            for(int i=0;i<adjList.get(curNode).size();i++)
-            {
-                int adjNode = adjList.get(curNode).get(i);
-                if(vis[adjNode]==0)
-                {
-                    indegree[adjNode]--;
-                    if(indegree[adjNode]==0)
-                    {
-                        vis[adjNode]=1;
-                        q.add(adjNode);
-                        topoSort.add(adjNode);
-                        count++;
-                    }
+        if(q.size()>1)
+            return false;
+
+        while(!q.isEmpty()){
+            int curNode = q.poll();
+            int count = 0;
+            HashSet<Integer> set  = adjList.get(curNode);
+            for(int adjNode:set){
+                indegree[adjNode]--;
+                if(indegree[adjNode]==0){
+                    toposort.add(adjNode);
+                    count++;
+                    q.add(adjNode);
                 }
-                if(count>1) return false;
+                if(count>1)
+                    return false;
             }
         }
-        
-        for(int i=0;i<length;i++)
-        {
-            if(nums[i]!=topoSort.get(i)) return false;
+
+        if(toposort.size()!=numberOfNodes)
+            return false;
+
+        for(int i=0;i<numberOfNodes;i++){
+            if(nums[i]!=toposort.get(i))
+                return false;
         }
 
         return true;
