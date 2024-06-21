3416abaf926b22d7dfa1ab878669420919974853
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-21 08:47:15 +0530

    Time: 6 ms (16.67%) | Memory: 46 MB (90.45%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        List<int[]> grumpyIndices = new ArrayList<>();
+        int length = customers.length;
+        int nonGrumpySum = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (grumpy[i] == 0) {
+                nonGrumpySum += customers[i];
+            } else {
+                grumpyIndices.add(new int[] { customers[i], i });
+            }
+        }
+        int maxCustomerSum = 0;
+        int runningCustomerSum = 0;
+        // for(int i=0;i<grumpyIndices.size();i++){
+        //     System.out.println(Arrays.toString(grumpyIndices.get(i)));
+        // }
+
+        int left = 0;
+        int right = 0;
+        length = grumpyIndices.size();
+        while (left <= right && right <= length) {
+
+            if (right < length &&
+                    grumpyIndices.get(right)[1] - grumpyIndices.get(left)[1] >= minutes) {
+                runningCustomerSum -= grumpyIndices.get(left)[0];
+                left++;
+                continue;
+            }
+
+            if (right < length) {
+                runningCustomerSum += grumpyIndices.get(right)[0];
+                maxCustomerSum = Math.max(maxCustomerSum, runningCustomerSum);
+            }
+            right++;
+        }
+
+        int maxCustomerSatisfaction = maxCustomerSum + nonGrumpySum;
+        return maxCustomerSatisfaction;
+
+    }
+}
