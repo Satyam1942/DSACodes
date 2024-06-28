f22fe4561c54a3c0795305f198eafa9c40cf06b8
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-28 08:32:55 +0530

    Time: 13 ms (95.34%) | Memory: 42.9 MB (69.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,49 +1,37 @@
 class Solution {
+    //Manacher's Algo
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        int centre = 0;
+        int right = 0;
+        int cache[] = new int[2*length+1];
+
+        s = s.replace("","#");
     
-   public String longestPalindrome(String s) {
-   int len = s.length();
-   int lowerBound=0,upperBound=0,maxi =0;
-   for(int centre =0;centre<len;centre++)
-   {
-        int left = centre;
-       int right=centre;
-        int count=0;
-       while(left>=0 && right<len)
-       {  
-         if(s.charAt(left)==s.charAt(right)) count++;
-           else break;
-            left--;
-            right++;
-       }
-    if(count>maxi)
-    {
-        maxi =count;
-        lowerBound = left+1;
-        upperBound = right-1;
-    }
-   }
+        for(int i=0;i<2*length+1;i++){
+            if(i<right){
+                cache[i] = Math.min(right-i,cache[centre-(i-centre)]);
+            }
 
-    for(int centre =0;centre<len;centre++)
-    {
-        int left = centre;
-       int right=centre+1;
-        int count=1;
-       while(left>=0 && right<len)
-       {  
-         if(s.charAt(left)==s.charAt(right)) count++;
-           else break;
-            left--;
-            right++;
-       }
-    if(count>maxi)
-    {
-        maxi =count;
-        lowerBound = left+1;
-        upperBound = right-1;
-    }
-   }
+            while(i+1+cache[i]<s.length() && i-1-cache[i]>=0 && s.charAt(i+1+cache[i])==s.charAt(i-1-cache[i])){
+                cache[i]++;
+            }
 
+            if(i+cache[i]>right){
+                right = i+cache[i];
+                centre = i;
+            }
+        }
 
-   return s.substring(lowerBound,upperBound+1);      
+        int maxIndex =0, maxLen = 0;
+        for(int i=0;i<cache.length;i++){
+            if(cache[i]>maxLen){
+                maxLen = cache[i];
+                maxIndex = i;
+            }
+        }
+
+        String  ans = s.substring(maxIndex-maxLen,maxIndex+maxLen+1).replace("#","");
+        return ans;
     }
 }
