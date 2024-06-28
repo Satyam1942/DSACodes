c6fca69fbaff07e51ee7a0cc8dd7af4a4da9d5c4
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-06-28 09:19:23 +0530

    Time: 25 ms (41.08%) | Memory: 75.1 MB (8.11%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long maximumImportance(int noOfCities, int[][] roads) {
+        int numberOfRoads = roads.length;
+        int indegree[][] = new int[noOfCities][2];
+
+        for(int i=0;i<noOfCities;i++){
+            indegree[i][1] = i;
+            indegree[i][1] = i;
+        }
+
+        for (int i = 0; i < numberOfRoads; i++) {
+            indegree[roads[i][1]][0]++;
+            indegree[roads[i][0]][0]++;
+        }
+
+        Arrays.sort(indegree, (a, b) -> (b[0] - a[0]));
+
+        int valOfNodes[] = new int[noOfCities];
+        int val = noOfCities;
+    
+        for (int i = 0; i < noOfCities; i++) {
+            int city = indegree[i][1];
+            valOfNodes[city] = val;
+            val--;
+        }
+
+        long maxImportance = 0;
+        for (int i = 0; i < numberOfRoads; i++) {
+            int cityA = roads[i][0];
+            int cityB = roads[i][1];
+            maxImportance += (long) valOfNodes[cityA] + (long) valOfNodes[cityB];
+        }
+
+        return maxImportance;
+
+    }
+}
