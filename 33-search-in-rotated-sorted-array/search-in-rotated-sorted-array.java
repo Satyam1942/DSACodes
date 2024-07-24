eda3dbcbeeca40e5f2ac8a00948e512318988a7b
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-07-24 11:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (58.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -4,21 +4,28 @@ class Solution {
         int right = nums.length-1;
         while(left<=right){
             int mid = (left+right)/2;
-            if(target==nums[mid])
+            if(nums[mid]==target)
                 return mid;
-
-            if(nums[left]<=nums[mid]){
-                if(target>=nums[left] && target<nums[mid])
-                    right = mid-1;
-                else 
+           
+            if(nums[mid]>nums[right]){
+                if(target>nums[mid])
                     left = mid+1;
-
-            }else {
-                if(target>nums[mid] && target<=nums[right])
-                    left = mid+1;
-                else
+                else {
+                    if(target<nums[left])
+                        left = mid+1;
+                    else
+                        right = mid-1;
+                }
+           }else{
+                if(target>nums[mid]){
+                    if(target<=nums[right])
+                        left = mid+1;
+                    else
+                        right = mid-1;
+                }else{
                     right = mid-1;
-            }
+                }
+           }
         }
 
         return -1;
