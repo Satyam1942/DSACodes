a8a845b028d754399ce23c34887770bab2b5473f
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-07-26 11:51:55 +0530

    Time: 34 ms (85.01%) | Memory: 46.9 MB (24.89%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,7 +1,9 @@
 class Solution {
     int minimumCuts(int index, String s, boolean palindromeCache[][], Integer cache[]){
+        
         if(index>=s.length()-1)
             return 0;
+
         if(cache[index]!=null)
             return cache[index];
 
@@ -9,8 +11,8 @@ class Solution {
         for(int k=index;k<s.length();k++){
             if(palindromeCache[index][k]){
                 int temp = minimumCuts(k+1,s,palindromeCache,cache);
-                if(k!=s.length()-1)
-                    temp+=1;
+                if(k+1!=s.length())
+                    temp++;
                 minCuts = Math.min(minCuts,temp);
             }
         }
@@ -24,7 +26,7 @@ class Solution {
         for(int center = 0; center<s.length();center++){
             int leftOdd = center;
             int rightOdd = center;
-            while(leftOdd>=0 && rightOdd<length){
+            while(leftOdd>=0 && rightOdd<length) {
                 if(s.charAt(leftOdd)==s.charAt(rightOdd))
                     palindromeCache[leftOdd][rightOdd] = true;
                 else{
