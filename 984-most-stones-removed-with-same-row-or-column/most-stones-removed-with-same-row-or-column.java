e185b2f00f7e130723483d8f257cb6db20bb863f
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-08-29 06:34:13 +0530

    Time: 59 ms (12.62%) | Memory: 45 MB (37.69%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,17 +1,19 @@
 class Solution {
-    void dfs( List<List<Integer>> adjList, int[] vis, int node)
-    {
+
+    void dfs( List<List<Integer>> adjList, int[] vis, int node) {
         vis[node]=1;
-         for(int adjNode: adjList.get(node))
-         {
-             if(vis[adjNode]==0) dfs(adjList, vis,adjNode);
-         }
+        for(int adjNode: adjList.get(node)) {
+            if(vis[adjNode]==0)   
+                dfs(adjList, vis,adjNode);
+        }
     }
+
     public int removeStones(int[][] stones) {
         int noOfStones = stones.length;
-       List<List<Integer>> adjList = new ArrayList<>();
+        List<List<Integer>> adjList = new ArrayList<>();
 
-        for(int i=0;i<noOfStones;i++) adjList.add(new ArrayList<>());
+        for(int i=0;i<noOfStones;i++) 
+            adjList.add(new ArrayList<>());
 
         for(int i=0;i<noOfStones;i++) {
             for(int j=0;j<noOfStones;j++) { 
