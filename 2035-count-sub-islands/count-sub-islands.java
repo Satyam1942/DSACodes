d99fa404327a4c90eccabf4de366424e14f80b20
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-08-29 06:27:01 +0530

    Time: 33 ms (23.83%) | Memory: 98 MB (5.06%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -1,32 +1,42 @@
 class Solution {
-   int ans =0;
-public void dfs(int[][] grid1, int[][] grid2, int i , int j){
-    if(i<0|| i>=grid1.length ||j<0|| j>=grid1[0].length || grid2[i][j]==0 ) return;
-    if(grid1[i][j]== 0)ans =0;
-    grid2[i][j] =0;
-    dfs(grid1, grid2, i-1, j);
-    dfs(grid1, grid2, i+1, j);
-    dfs(grid1, grid2, i, j+1);
-    dfs(grid1, grid2, i, j-1);
-}
+    boolean isSubIsland = true;
+    
+    void dfs(int curRow, int curCol, int vis[][], int grid1[][] , int grid2[][]){
+        vis[curRow][curCol]  = 1;
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,1,0,-1};
+        int length = grid1.length;
+        int width = grid1[0].length;
 
-
-
-public int countSubIslands(int[][] grid1, int[][] grid2) {
-    // problem can be solved using dfs
-    int res=0;
-    for(int i=0 ;i <grid1.length ;i++){
-        for(int j=0 ;j<grid2[0].length ;j++){
-            
-            if(grid2[i][j]==1)
-            {
-                ans=1;
-                dfs(grid1, grid2, i, j);
-                res+=ans;
+        for(int i=0;i<4;i++) {
+            int adjRow = curRow + delRow[i];
+            int adjCol = curCol + delCol[i];
+            boolean isValid = adjRow>=0 && adjRow<length && adjCol>=0 && adjCol<width;
+            if(isValid && vis[adjRow][adjCol]==0 && grid2[adjRow][adjCol]==1){
+                dfs(adjRow,adjCol,vis,grid1,grid2);
+                if(grid1[adjRow][adjCol]==0)
+                    isSubIsland = false;
             }
         }
     }
-    return res;
-    
-}
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int length = grid1.length;
+        int width = grid1[0].length;
+        int vis[][] = new int[length][width];
+
+        int count = 0;
+        for(int i=0;i<length;i++){
+            for(int j=0;j<width;j++){
+                if(vis[i][j]==0 && grid2[i][j]==1 && grid1[i][j]==1){
+                    isSubIsland = true;
+                    dfs(i,j,vis,grid1,grid2);
+                    if(isSubIsland)
+                        count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
