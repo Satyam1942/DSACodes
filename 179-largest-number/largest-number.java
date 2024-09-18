b572c398ce45e75e44d6899612e9d0c1b016eb68
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-09-18 08:09:13 +0530

    Time: 7 ms (26.06%) | Memory: 44.1 MB (18.79%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,40 @@
+class CustomComparator implements Comparator<String>{
+    public int compare(String a, String b){
+        String c = a+b;
+        String d = b+a;
+        if(c.equals(d))
+            return -1;
+        long valC = Long.valueOf(c);
+        long valD = Long.valueOf(d);
+
+        return Long.compare(valD,valC);
+    }
+}
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        int length = nums.length;
+        String numStr[] = new String[length];
+        for(int i=0; i<length;i++){
+            numStr[i] =  String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(numStr, new CustomComparator());
+
+        StringBuilder largestNumber = new StringBuilder();
+        for(int i=0;i<length;i++){
+            largestNumber.append(numStr[i]);
+        }
+
+        String newStr =  largestNumber.toString();
+        if(newStr.charAt(0)=='0')
+            return "0";
+        else 
+            return newStr;
+    }
+}
+
+/*
+    32, 30 , 34
+    34,32,30
+*/
