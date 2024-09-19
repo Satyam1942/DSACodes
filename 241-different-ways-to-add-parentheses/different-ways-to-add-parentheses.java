f8c575fcfcc0d1ef631dc7f9a99348b03a9478f3
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-09-19 22:32:47 +0530

    Time: 3 ms (24.50%) | Memory: 41.9 MB (78.62%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -0,0 +1,77 @@
+class Pair {
+    int num;
+    char operator;
+    Pair(int num, char operator){
+        this.num = num;
+        this.operator = operator;
+    }
+}
+
+class Solution {
+    List<Integer> results = new ArrayList<>();
+    public boolean isDigit(char token){
+        return (int)token>=48 && (int)token<=57;
+    }
+    public void compute(int index, String expression, Stack<Pair> unevaluated) {
+
+        int curNum = (int)expression.charAt(index)-48;
+        if(index+1<expression.length() && isDigit(expression.charAt(index+1))) {
+            curNum*=10;
+            index++;
+            curNum+=(int)expression.charAt(index)-48;
+        }
+ 
+        if(index+1==expression.length()) {
+            Stack<Pair> temp = (Stack<Pair>)unevaluated.clone();
+            while(!temp.isEmpty()) {
+                char operator = temp.peek().operator;
+                int prevNum = temp.peek().num;
+                temp.pop();
+                if(operator=='+')
+                    curNum = prevNum+curNum;
+                else if(operator == '-')
+                    curNum = prevNum-curNum;
+                else if(operator == '*')
+                    curNum = prevNum*curNum;
+            }
+            results.add(curNum);
+            return;
+        }
+
+
+        char nextOperator = expression.charAt(index+1);
+
+        //not evaluating
+        unevaluated.push(new Pair(curNum,nextOperator));
+        compute(index+2,expression,unevaluated);
+        unevaluated.pop();
+        
+        Stack<Pair> temp = (Stack<Pair>)unevaluated.clone();
+        //evaluating
+        while(!temp.isEmpty()){
+            char operator =  temp.peek().operator;
+            int prevNum = temp.peek().num;
+            temp.pop();
+            if(operator=='+')
+                curNum = prevNum+curNum;
+            else if(operator == '-')
+                curNum = prevNum-curNum;
+            else if(operator == '*')
+                curNum = prevNum*curNum;
+            
+            temp.push(new Pair(curNum,nextOperator));
+            compute(index+2,expression,temp);
+            temp.pop();
+        }
+    }
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        Stack<Pair> unevaluated = new Stack<>();
+        compute(0,expression,unevaluated);
+        return results;
+    }
+}
+
+/*
+    2*3-4*5
+*/
