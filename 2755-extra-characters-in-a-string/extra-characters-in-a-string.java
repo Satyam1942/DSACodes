87f3e1c055e639787fb0059194f90a416724fa48
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-09-23 07:12:43 +0530

    Time: 43 ms (68.37%) | Memory: 45.4 MB (36.39%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,35 +1,44 @@
 class Solution {
-    int rec(String s , int i , int j , HashSet<String> dictionarySet,int dp[][])
-    {
-        if(i==j)
-        {
-            if(dictionarySet.contains(s.substring(i,j+1))) return 0;
-            else return 1;
+    void addWordsToSet(String[] dictionary, HashSet<String> dictionarySet){
+        for(String word : dictionary) {
+            dictionarySet.add(word);
         }
-       
-        if(dictionarySet.contains(s.substring(i,j+1))) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int countLeft =0 , countRight = 0, mini = Integer.MAX_VALUE;
-        for(int k=i;k<j;k++)
-        {
-            String leftSubstr = s.substring(i,k+1);
-            if(!dictionarySet.contains(leftSubstr)) countLeft = leftSubstr.length();
-            else countLeft = 0;
-            countRight = rec(s,k+1,j,dictionarySet,dp);
-            mini = Math.min(mini , countRight+countLeft);
-        }
-
-        return dp[i][j] =  mini;
     }
-    public int minExtraChar(String s, String[] dictionary) {
-        HashSet<String> dictionarySet = new HashSet<>();
-        int len = s.length();
-        for(int i=0;i<dictionary.length;i++) dictionarySet.add(dictionary[i]);
 
-        int dp[][] = new int[len][len];
-        for(int row[] :dp) Arrays.fill(row,-1);
-        return rec(s,0,s.length()-1,dictionarySet,dp);
+    int partitionString(int i, String word, HashSet<String> dictionarySet, Integer cache[]) {
+        
+        if(i==word.length())
+            return 0;
+
+        if(cache[i]!=null)
+            return cache[i];
+
+        int notUse = 1+partitionString(i+1, word, dictionarySet, cache);
+        int use = Integer.MAX_VALUE;
+
+        for(int k=i; k<word.length(); k++) {
+            String subWord = word.substring(i,k+1);
+            if(dictionarySet.contains(subWord)) {
+                int temp = partitionString(k+1, word, dictionarySet, cache);
+                use = Math.min(use, temp);
+            } 
+        }
+
+        return cache[i] = Math.min(use, notUse);
+    }
+
+    public int minExtraChar(String word, String[] dictionary) {
+        HashSet<String> dictionarySet = new HashSet<>();
+        int lengthOfWord = word.length();
+        Integer cache[] = new Integer[lengthOfWord];
+
+        addWordsToSet(dictionary, dictionarySet);
+
+        return partitionString(0, word, dictionarySet, cache);
 
     }
 }
+
+/*
+
+ */
