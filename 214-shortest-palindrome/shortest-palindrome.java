b88515bb085bf34854259fddcf63d4acf36816a2
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-09-23 22:56:03 +0530

    Time: 4 ms (90.30%) | Memory: 44.5 MB (83.44%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,12 +1,67 @@
 class Solution {
-   public String shortestPalindrome(String s) {
-    int n = s.length(), pos = -1;
-    long B = 29, MOD = 1000000007, POW = 1, hash1 = 0, hash2 = 0;
-    for (int i = 0; i < n; i++, POW = POW * B % MOD) {
-        hash1 = (hash1 * B + s.charAt(i) - 'a' + 1) % MOD;
-        hash2 = (hash2 + (s.charAt(i) - 'a' + 1) * POW) % MOD;
-        if (hash1 == hash2) pos = i;
+    void computeForwardHash(String s, long forwardHash[]) {
+        int length = s.length();
+        long base = 29;
+        long mod = (long)(1e9+7);
+
+        for(int i=0; i<length; i++) {
+            char token = s.charAt(i);
+            int index = (int)token-97;
+            forwardHash[i] = (i==0)? index : (forwardHash[i-1]*base + index)%mod;
+        }
     }
-    return new StringBuilder().append(s.substring(pos + 1, n)).reverse().append(s).toString();
+
+    void computeBackwardHash(String s, long backwardHash[]) {
+        int length = s.length();
+        long base = 29;
+        long power = 1;
+        long mod = (long)(1e9+7);
+
+        for(int i=0; i<length; i++) {
+            char token = s.charAt(i);
+            int index = (int)token-97;
+            backwardHash[i] = (i==0)? index : (backwardHash[i-1] + index*power)%mod;
+            power = (power*base)%mod;
+        }
+    }
+
+    int getLongestPalindromicPrefixIndex(long forwardHash[] , long backwardHash[]) {
+        int length = forwardHash.length;
+        int maxIndex = 0;
+        for(int i=0; i<length; i++) {
+            if(forwardHash[i]==backwardHash[i]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public String shortestPalindrome(String s) {
+        int length = s.length();
+        if(length==0 || length==1)
+            return s;
+
+        long forwardHash[] = new long[length];
+        long backwardHash[] = new long[length];
+
+        computeForwardHash(s, forwardHash);
+        computeBackwardHash(s, backwardHash);
+
+        int prefixIndex = getLongestPalindromicPrefixIndex(forwardHash, backwardHash);
+        StringBuilder shortestPalindrome = new StringBuilder(s.substring(prefixIndex+1));
+
+        shortestPalindrome.reverse();
+        shortestPalindrome.append(s);
+
+        return shortestPalindrome.toString();
+    }
 }
-}
+
+/*
+    a a  c   e c  a     a        a
+    1 11 113 1135 11351 113511   1135111
+
+    aa a        c      e        c      a      a
+              35311   5311     311     11     1 
+
+*/
