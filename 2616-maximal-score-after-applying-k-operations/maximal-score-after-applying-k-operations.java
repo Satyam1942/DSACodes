81d88a25179b926dc2b7b512a12407d8a7ab8edf
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-10-14 06:19:11 +0530

    Time: 136 ms (65.88%) | Memory: 63 MB (49.76%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -9,10 +9,15 @@ class Solution {
         
         while(k-->0) {
             int curScore = pq.poll();
+            if(curScore==1) {
+                break;
+            }
             int newScore = (int)Math.ceil(curScore/3.0);
             pq.add(newScore);
             score+=curScore;
         }
+
+        score+=(k+1);
 
         return score;
     }
