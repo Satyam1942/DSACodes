f93cb57af45dc07c72113ed8db63cc48cbcc3a9a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-10-18 22:44:32 +0530

    Time: 11 ms (51.14%) | Memory: 65.9 MB (5.26%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,5 +1,5 @@
 class Solution {
-    int recursion(int index, int curBitOr, int maxBitOr, int nums[]) {
+    int recursion(int index, int curBitOr, int maxBitOr, int nums[], Integer cache[][]) {
         if(index == nums.length) {
             if(curBitOr==maxBitOr) {
                 return 1;
@@ -8,18 +8,25 @@ class Solution {
             }
         }
 
-        int notTakeOr = recursion(index+1, curBitOr, maxBitOr, nums);
-        int takeOr = recursion(index+1, curBitOr|nums[index], maxBitOr, nums);
+        if(cache[index][curBitOr]!=null) {
+            return cache[index][curBitOr];
+        }
 
-        return takeOr+notTakeOr;
+        int notTakeOr = recursion(index+1, curBitOr, maxBitOr, nums, cache);
+        int takeOr = recursion(index+1, curBitOr|nums[index], maxBitOr, nums, cache);
+
+        return cache[index][curBitOr] = takeOr+notTakeOr;
     }
 
     public int countMaxOrSubsets(int[] nums) {
         int maxBitOr = 0;
         int length = nums.length;
+
         for(int i = 0;i<length;i++) {
             maxBitOr |= nums[i]; 
         }
-        return recursion(0, 0, maxBitOr, nums);
+        
+        Integer cache[][] = new Integer[length+1][maxBitOr+1];
+        return recursion(0, 0, maxBitOr, nums, cache);
     }
 }
