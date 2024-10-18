c7592ce0c7c011129b5ba27d36a460a5e3c05876
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-10-18 22:42:53 +0530

    Time: 8 ms (86.00%) | Memory: 40.8 MB (85.96%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    int recursion(int index, int curBitOr, int maxBitOr, int nums[]) {
+        if(index == nums.length) {
+            if(curBitOr==maxBitOr) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        int notTakeOr = recursion(index+1, curBitOr, maxBitOr, nums);
+        int takeOr = recursion(index+1, curBitOr|nums[index], maxBitOr, nums);
+
+        return takeOr+notTakeOr;
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxBitOr = 0;
+        int length = nums.length;
+        for(int i = 0;i<length;i++) {
+            maxBitOr |= nums[i]; 
+        }
+        return recursion(0, 0, maxBitOr, nums);
+    }
+}
