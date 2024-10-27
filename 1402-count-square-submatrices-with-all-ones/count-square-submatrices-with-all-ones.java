0581c6faff89119125111651203e75af560754ce
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-10-27 19:43:43 +0530

    Time: 7 ms (45.12%) | Memory: 55.1 MB (38.46%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -4,42 +4,40 @@ class Solution {
         int n = matrix[0].length;
         int dp[][] = new int[m][n];
 
-        for(int i=0;i<n;i++) 
-        {
-            if(matrix[0][i]==1) dp[0][i]= 1;
+        for(int i=0;i<n;i++) {
+            if(matrix[0][i]==1) {
+                dp[0][i]= 1;
+            }
         }
 
-        for(int j=0;j<m;j++) 
-        {
-            if(matrix[j][0]==1) dp[j][0] =1;
+        for(int j=0;j<m;j++) {
+            if(matrix[j][0]==1) {
+                dp[j][0] =1;
+            }
         }
 
         int delRow []={-1,0,-1};
         int delCol [] = {-1,-1,0};
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) {
                 int mini = Integer.MAX_VALUE;
-                for(int k=0;k<3;k++)
-                {
+                for(int k=0;k<3;k++) {
                     int newRow = i+delRow[k];
                     int newCol = j+delCol[k];
-                   
                     mini = Math.min(mini,dp[newRow][newCol]);
+                }         
+                if(matrix[i][j]==1) {
+                    dp[i][j]=mini+1;
+                } 
+                else {
+                    dp[i][j] =0;
                 }
-                
-                if(matrix[i][j]==1) dp[i][j]=mini+1;
-                else dp[i][j] =0;
-                
             }
         }
 
         int sum=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
                 sum+=dp[i][j];
             }
         }
