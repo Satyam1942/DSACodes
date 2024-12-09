ec7b9d2de2830c73e427172146fd07b2af755b49
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-12-09 08:53:06 +0530

    Time: 3 ms (86.83%) | Memory: 98 MB (88.02%) - LeetSync

--- 3427-special-array-ii/special-array-ii.java
+++ 3427-special-array-ii/special-array-ii.java
@@ -1,35 +1,52 @@
 class Solution {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        
+        int length = nums.length;
+        int prefixAns[] = new int[length-1];
+        boolean ans[] = new boolean[queries.length];
         boolean isEven = nums[0]%2==0;
-       
-        for(int i=1;i<nums.length;i++){
+        int j = 0;
+
+        for(int i=1;i<length;i++)  {
             if(isEven) {
-                if(nums[i]%2==0)
-                    map.put(i-1,i);
-                else
-                    isEven=!isEven;
-            } else {
                 if(nums[i]%2!=0) {
-                     map.put(i-1,i);
+                    prefixAns[j] = (j==0)?1:prefixAns[j-1]+1;
+                    isEven = !isEven;
+                } else {
+                    prefixAns[j] = 0;
                 }
-                else {
-                    isEven=!isEven;
+            } else {
+                if(nums[i]%2==0) {
+                    prefixAns[j] = (j==0)?1:prefixAns[j-1]+1;
+                    isEven = !isEven;
+                } else {
+                    prefixAns[j] = 0;
                 }
             }
+            j++;
         }
-        
-        boolean ans[] = new boolean[queries.length];
+
         for(int i=0;i<queries.length;i++){
             int l = queries[i][0];
             int r = queries[i][1];
-            Integer key = map.ceilingKey(l);
-            if(key!=null && key<=r && key+1<=r){
+            if(l==r) {
+                ans[i] = true;
+                continue;
+            }
+            int count = (l==0)? prefixAns[r-1]: prefixAns[r-1]-prefixAns[l-1];
+           
+            if(count!=(r-l)){
                 ans[i] = false;
-            }else
+            } else
                 ans[i] =true;
         }
         return ans;
     }
 }
+
+/*
+    3, 4, 1, 2, 6
+     1   2  3  0
+
+     4,3,1,6
+      1 0 1
+*/
