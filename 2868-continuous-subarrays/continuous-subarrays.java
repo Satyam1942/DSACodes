c4deb712d0f7c0e1a9f985694438af9a07350861
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2024-12-14 09:40:33 +0530

    Time: 33 ms (41.41%) | Memory: 58.3 MB (67.14%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.java
+++ 2868-continuous-subarrays/continuous-subarrays.java
@@ -1,29 +1,35 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        int n = nums.length;
+        int length = nums.length;
         long noOfSubarrays=0;
         TreeMap<Integer,Integer> map = new TreeMap<>();
-        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
-        int right =0,left=0;
-     
-     
-      while(left<n) {
-            mini =Math.min(mini,nums[left]);
-            maxi = Math.max(maxi,nums[left]);
-            map.put(nums[left],map.getOrDefault(nums[left],0)+1);
-            
-             while(Math.abs(maxi-mini)>2) {
-                map.put(nums[right], map.get(nums[right])-1);
-                if(map.get(nums[right])==0) map.remove(nums[right]);
-                mini =map.firstKey();
-                maxi = map.lastKey();
-               right++;
+        int mini = 0, maxi = 0;
+        int right = 0, left = 0;
+
+        while(right<=length) {
+            mini = (map.isEmpty())? 0 : map.firstKey();
+            maxi = (map.isEmpty())? 0 : map.lastKey();
+
+            if(maxi-mini>2) {
+                int leftFreq = map.get(nums[left]);
+                if(leftFreq==1) {
+                    map.remove(nums[left]);
+                } else {
+                    map.put(nums[left],leftFreq-1);
+                }
+                left++;
+                continue;
+            } else {
+                noOfSubarrays+=right-left;
             }
-              noOfSubarrays+=(left-right+1); 
             
-             left++;
-      }
-        
+            if(right<length) {
+                int rightFreq = map.getOrDefault(nums[right],0);
+                map.put(nums[right],rightFreq+1);
+            }
+            right++;
+        }
+
         return noOfSubarrays;
     }
 }
