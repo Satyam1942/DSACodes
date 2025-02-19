b811afa66dd206a66d62310701d91932c84172af
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-02-19 12:34:21 +0530

    Time: 1 ms (99.09%) | Memory: 41.4 MB (86.10%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,64 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        if(k>(3*(1<<(n-1)))) {
+            return "";
+        }
+    
+        StringBuilder sb = new StringBuilder();
+
+        if(n==1) {
+            sb.append((char)(k+96));
+            return sb.toString();
+        }
+        
+        k--;
+        
+        int index  = k/(1<<(n-1));
+        char token = (char)(index+97);
+        char prev = token;
+        sb.append(token);
+        k = k%(1<<(n-1));
+        n--;
+
+        while(n>0) {
+            index  = k/(1<<(n-1));
+            // System.out.println(index);
+            if(index==0) {
+                if(prev=='a') {
+                    token = 'b';
+                } else {
+                    token = 'a';
+                }
+            } else {
+                if(prev=='c') {
+                    token = 'b';
+                } else {
+                    token = 'c';
+                }
+            }
+
+            prev = token;
+            sb.append(token);
+            k = k%(1<<(n-1));
+            n--;
+        }
+
+        return sb.toString();
+    }
+}
+
+/*
+ k/2^n-1
+ k = k%2^n-1 ; n--;
+
+ 9/4  = 2 => c
+ k = 1, n = 2;
+ 1/2 =  0 => a
+ k = 1 ; n = 1
+ 1/1 = 1 =>b
+ k = 0 ; n = 0;
+ break; 
+
+
+
+*/
