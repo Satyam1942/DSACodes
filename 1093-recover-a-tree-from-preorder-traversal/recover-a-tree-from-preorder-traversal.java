7c4f58dedbdd47078c710a685529f1601b9c4260
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-02-22 17:42:12 +0530

    Time: 11 ms (11.72%) | Memory: 44.9 MB (54.95%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -13,34 +13,100 @@
  *     }
  * }
  */
-class Solution {
-        public TreeNode recoverFromPreorder(String S) {
-        int level, val;
-        Stack<TreeNode> stack = new Stack<>();
-        for (int i = 0; i < S.length();) {
-            for (level = 0; S.charAt(i) == '-'; i++) {
-                level++;
-            }
-            for (val = 0; i < S.length() && S.charAt(i) != '-'; i++) {
-                val = val * 10 + (S.charAt(i) - '0');
-            }
-            // level is one less than height
-            while (stack.size() > level) {
-                stack.pop();
-            }
-            TreeNode node = new TreeNode(val);
-            if (!stack.isEmpty()) {
-                if (stack.peek().left == null) {
-                    stack.peek().left = node;
-                } else {
-                    stack.peek().right = node;
-                }
-            }
-            stack.add(node);
-        }
-        while (stack.size() > 1) {
-            stack.pop();
-        }
-        return stack.pop();
+class Pair{
+    TreeNode node;
+    int depth;
+    Pair(TreeNode node, int depth){
+        this.node = node;
+        this.depth = depth;
+    }
+    @Override
+    public String toString(){
+        return "("+node.val+","+depth+")";
     }
 }
+
+class Solution {
+    public TreeNode recoverFromPreorder(String traversal) {
+        List<Pair> preorderTraversal = new ArrayList<>();
+        int depth =0;
+        int length = traversal.length();
+        StringBuilder number = new StringBuilder();
+
+        for(int i=0;i<length;){
+            char token = traversal.charAt(i);
+            while(i<length && token>=48 && token<=57){
+                token = traversal.charAt(i);
+                if(token=='-'){
+                    TreeNode newNode = new TreeNode(Integer.valueOf(number.toString()),null,null);
+                    number = new StringBuilder();
+                    preorderTraversal.add(new Pair(newNode,depth));
+                    depth = 0;
+                    break;
+                }
+                number.append(token-48);
+                i++;
+            }
+            while(i<length && token=='-'){
+                token = traversal.charAt(i);
+                if(token!='-')
+                    break;
+                depth++;
+                i++;
+            }
+        }
+
+        if(!number.toString().equals("")){
+        TreeNode newNode = new TreeNode(Integer.valueOf(number.toString()),null,null);
+         preorderTraversal.add(new Pair(newNode,depth));
+        }
+        
+        HashMap<Integer,Queue<TreeNode>> map = new HashMap<>();
+        for(int i=0;i<preorderTraversal.size();i++){
+            depth = preorderTraversal.get(i).depth;
+            TreeNode node = preorderTraversal.get(i).node;
+            if(!map.containsKey(depth))
+                map.put(depth,new LinkedList<>());
+            map.get(depth).add(node);
+        }
+
+        TreeNode head = preorderTraversal.get(0).node;
+        // System.out.println(preorderTraversal);
+        for(int i=1;i<preorderTraversal.size();i++){
+            TreeNode curNode = preorderTraversal.get(i).node;
+            int curDepth = preorderTraversal.get(i).depth;
+            // System.out.println(curDepth);
+            TreeNode parent = map.get(curDepth-1).peek();
+            if(parent.left!=null){
+                parent.right = curNode;
+                map.get(curDepth-1).poll();
+            }else
+                parent.left = curNode;
+            
+            while(map.get(curDepth).peek()!=curNode)
+                map.get(curDepth).poll();
+        }
+
+        return head;
+    }
+}
+/*
+    (1,0)
+    (2,1) ; 
+    (3,2) ;
+    (5,1)
+    (6,2)
+    (9,3)
+    (7,2)
+
+    0->
+    1->2,5
+    2->3,4,6,7
+    3-> 8
+
+       1
+    2        5
+ 3     4   6    7
+
+ whenever I go to smaller level in my list i  need to make sure that previous nodes were polled from queue
+ */
