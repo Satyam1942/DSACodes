e8994fe91c21f38cfc694e76bfc5713310877289
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-03-11 07:03:27 +0530

    Time: 32 ms (23.08%) | Memory: 45.6 MB (6.87%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,39 +1,39 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int left = 0;
-        int right = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
         int length = s.length();
-        int count = 0;
-        while(left<=right && right<=length){
-            if(map.size()==3){
-                count += length-right+1;
-                int freq = map.get(s.charAt(left));
-                if(freq==1)
-                    map.remove(s.charAt(left));
-                else
-                    map.put(s.charAt(left),freq-1);
-                left++;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int substrCount = 0;
+
+        while(j<=length) {
+            if(map.size()==3) {
+                substrCount+= length-j+1;
+                char prevToken = s.charAt(i);
+                int freq = map.get(prevToken);
+                if(freq==1) {
+                    map.remove(prevToken);
+                } 
+                else {
+                    map.put(prevToken, freq-1);
+                }
+
+                i++;
                 continue;
             }
 
-            if(right<length){
-                char token = s.charAt(right);
-                int freq=  map.getOrDefault(token,0);
-                map.put(token,freq+1);
+            if(j<length) {
+                char token = s.charAt(j);
+                int curFreq = map.getOrDefault(token,0);
+                map.put(token, curFreq+1);
             }
-            right++;
+            j++;
         }
-
-        return count;
+        return substrCount;
     }
 }
 
-/* 
-          r
+/*
     abcabc
-       l
-
-    a,c,b
     4+3+2+1
 */
