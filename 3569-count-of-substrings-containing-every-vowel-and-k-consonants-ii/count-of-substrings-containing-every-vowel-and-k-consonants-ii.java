a8083e7d1043a434dc4c535451ebf181f4ad2cc6
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-03-11 08:09:28 +0530

    Time: 785 ms (5.13%) | Memory: 66 MB (5.13%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,46 +1,69 @@
 class Solution {
+    boolean isVowel(char token) {
+        return token=='a' || token == 'e' || token=='i' || token =='o' || token=='u';
+    }
+
     public long countOfSubstrings(String word, int k) {
-        int[][] frequencies = new int[2][128];
-        frequencies[0]['a'] = 1;
-        frequencies[0]['e'] = 1;
-        frequencies[0]['i'] = 1;
-        frequencies[0]['o'] = 1;
-        frequencies[0]['u'] = 1;
-
-        long response = 0;
-        int currentK = 0, vowels = 0, extraLeft = 0;
-
-        for (int right = 0, left = 0; right < word.length(); right++) {
-            char rightChar = word.charAt(right);
-
-            if (frequencies[0][rightChar] == 1) {
-                if (++frequencies[1][rightChar] == 1) vowels++;
-            } else {
-                currentK++;
-            }
-
-            while (currentK > k) {
-                char leftChar = word.charAt(left);
-                if (frequencies[0][leftChar] == 1) {
-                    if (--frequencies[1][leftChar] == 0) vowels--;
-                } else {
-                    currentK--;
-                }
-                left++;
-                extraLeft = 0;
-            }
-
-            while (vowels == 5 && currentK == k && left < right && frequencies[0][word.charAt(left)] == 1 && frequencies[1][word.charAt(left)] > 1) {
-                extraLeft++;
-                frequencies[1][word.charAt(left)]--;
-                left++;
-            }
-
-            if (currentK == k && vowels == 5) {
-                response += (1 + extraLeft);
+        int length = word.length();
+        HashMap<Character, Integer> vowels = new HashMap<>();
+        TreeSet<Integer> consonantIndices = new TreeSet<>();
+        for(int l=0; l<length; l++) {
+            char token = word.charAt(l);
+            if(!isVowel(token)) {
+                consonantIndices.add(l);
             }
         }
+        
+        int i = 0;
+        int j = 0;
+        int consonants = 0;
+        int lastConsonantIndex = 0;
+        long substrCount = 0;
 
-        return response;
+        while(j<=length) {
+            if(consonants >= k && vowels.size()==5) {
+                Integer nextIndex = consonantIndices.higher( lastConsonantIndex );
+                if(nextIndex==null) {
+                    nextIndex = length;
+                }
+
+                if(consonants == k) {
+                    substrCount+= nextIndex-j+1;
+                }
+                
+                char prevToken = word.charAt(i);
+                if(isVowel(prevToken)) {
+                    int freq = vowels.get(prevToken);
+                    if(freq == 1) {
+                        vowels.remove(prevToken);
+                    } else {
+                        vowels.put(prevToken, freq-1);
+                    }
+                } else {
+                    consonants--;
+                }
+                i++;
+                continue;
+            }
+
+            if(j<length) {
+                char token = word.charAt(j);
+                if(isVowel(token)) {
+                    int freq = vowels.getOrDefault(token, 0);
+                    vowels.put(token, freq+1);
+                } else {
+                    consonants++;
+                    lastConsonantIndex = j;
+                }
+            }
+            j++;
+        }
+
+        return substrCount;         
     }
 }
+
+/*
+    aeiaoule
+    2+2+1+
+*/
