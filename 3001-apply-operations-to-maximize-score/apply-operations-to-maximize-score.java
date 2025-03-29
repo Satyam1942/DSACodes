11e2ede5185e8d2d7e5d7a82e0c5515081bb4317
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-03-29 23:16:36 +0530

    Time: 284 ms (90.48%) | Memory: 61.8 MB (44.44%) - LeetSync

--- 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.java
+++ 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.java
@@ -1,79 +1,105 @@
 class Solution {
-    private final int mod = (int) 1e9 + 7;
+    int calculateScore(int num) {
+        int score = 0;
+        for(int i = 2; i*i<=num; i++) {
+            boolean flag = false;
+            while(num%i==0) {
+                flag = true;
+                num/=i;
+            }
+            if(flag) {
+                score++;
+            }
+        }
+
+        if(num>1) {
+            score++;
+        }
+
+        return score;
+    }
+
+    void computeRepeatLimit(List<Integer> nums, int score[], PriorityQueue<long[]> scoreMap) {
+        Stack<Integer> st = new Stack<>();
+        int length = nums.size();
+        for(int i=0; i<length; i++) {
+            while(!st.isEmpty() && score[st.peek()]<score[i]) {
+                int index = st.peek();
+                int val = nums.get(index);
+                st.pop();
+                int leftLimit = i-index;
+                int rightLimit = (st.isEmpty())? index+1: index-st.peek();
+                scoreMap.add(new long[]{val, (long)leftLimit*(long)rightLimit});
+            }
+            st.push(i);
+        }
+        
+        while(!st.isEmpty()) {
+            int index = st.peek();
+            int val = nums.get(index);
+            st.pop();
+            int leftLimit = length - index;
+            int rightLimit = (st.isEmpty())? index+1: index-st.peek();
+            scoreMap.add(new long[]{val, (long)leftLimit*(long)rightLimit});
+        }
+    }
+
+    long calcPow(long x, long power) {
+        long ans = 1l;
+        long mod = (long)(1e9+7);
+        for (int i = 0; i < 31; i++) {
+            if ((power & (1 << i)) > 0) {
+                ans =  (ans*x)%mod;
+            }
+            x = (x*x)%mod;
+        }
+        return ans;
+    }
 
     public int maximumScore(List<Integer> nums, int k) {
-        int n = nums.size();
-        int[][] arr = new int[n][0];
-        for (int i = 0; i < n; ++i) {
-            arr[i] = new int[] {i, primeFactors(nums.get(i)), nums.get(i)};
+        int length = nums.size();
+        int score [] = new int[length];
+        PriorityQueue<long[]> scoreMap = new PriorityQueue<>((a,b)->(Long.compare(b[0],a[0])));
+        
+        for(int i=0; i<length; i++) {
+            score[i] = calculateScore(nums.get(i));
         }
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(left, -1);
-        Arrays.fill(right, n);
-        Deque<Integer> stk = new ArrayDeque<>();
-        for (int[] e : arr) {
-            int i = e[0], f = e[1];
-            while (!stk.isEmpty() && arr[stk.peek()][1] < f) {
-                stk.pop();
-            }
-            if (!stk.isEmpty()) {
-                left[i] = stk.peek();
-            }
-            stk.push(i);
-        }
-        stk.clear();
-        for (int i = n - 1; i >= 0; --i) {
-            int f = arr[i][1];
-            while (!stk.isEmpty() && arr[stk.peek()][1] <= f) {
-                stk.pop();
-            }
-            if (!stk.isEmpty()) {
-                right[i] = stk.peek();
-            }
-            stk.push(i);
-        }
-        Arrays.sort(arr, (a, b) -> b[2] - a[2]);
-        long ans = 1;
-        for (int[] e : arr) {
-            int i = e[0], x = e[2];
-            int l = left[i], r = right[i];
-            long cnt = (long) (i - l) * (r - i);
-            if (cnt <= k) {
-                ans = ans * qpow(x, cnt) % mod;
-                k -= cnt;
-            } else {
-                ans = ans * qpow(x, k) % mod;
-                break;
-            }
-        }
-        return (int) ans;
-    }
 
-    private int primeFactors(int n) {
-        int i = 2;
-        Set<Integer> ans = new HashSet<>();
-        while (i <= n / i) {
-            while (n % i == 0) {
-                ans.add(i);
-                n /= i;
-            }
-            ++i;
-        }
-        if (n > 1) {
-            ans.add(n);
-        }
-        return ans.size();
-    }
+        computeRepeatLimit(nums, score, scoreMap);
 
-    private int qpow(long a, long n) {
-        long ans = 1;
-        for (; n > 0; n >>= 1) {
-            if ((n & 1) == 1) {
-                ans = ans * a % mod;
-            }
-            a = a * a % mod;
+        long product = 1l;
+        long mod = (long)(1e9+7);
+        while(k>0) {
+            long num = scoreMap.peek()[0];
+            long limit = scoreMap.peek()[1];
+            scoreMap.poll();
+
+            long pow = calcPow(num, Math.min(limit,(long)k));
+            product = (product*pow)%mod;
+            k-=limit;
         }
-        return (int) ans;
-    }
+
+        return (int)product;
+    }   
 }
+
+/*
+    score = 1
+        19  12  14  6   10  18
+    x   1   2   2   2   2   2   x
+
+    19 = 1
+    18 = 1
+    14 = 4
+    12 = 6
+    10 = 2
+    6  = 3
+    for constructing this find next greater and subtract from curIndex
+    and sort the list in terms of value from largest to smalles
+
+    a particular number can be part of array :
+    1. a = pos of a number whose score is more ahead - curPos+1
+    2. num^(min(k,a)) ;
+
+    19*18*14
+*/
