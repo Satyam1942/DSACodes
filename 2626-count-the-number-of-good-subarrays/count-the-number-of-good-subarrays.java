8d841bdf31c7e6a16829a661c6489a50d143dfd7
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-04-17 07:56:54 +0530

    Time: 37 ms (90.70%) | Memory: 58.4 MB (68.44%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
@@ -1,24 +1,34 @@
 class Solution {
     public long countGood(int[] nums, int k) {
-        int j=0;
-        int i=0;
-        long ans=0l;
-        long count=0l;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        while(j<nums.length){
-            hm.put(nums[j],hm.getOrDefault(nums[j],0)+1);
-            count += hm.get(nums[j]) - 1;
-            while(count>=k){
-                ans += (nums.length - j);
-                int lf = hm.get(nums[i]);
-                count-=lf-1;
-                hm.put(nums[i], hm.get(nums[i]) - 1);
-                if(hm.get(nums[i])==0)
-                    hm.remove(nums[i]);              
-                i++;
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        long count = 0;
+        long pairCount = 0;
+
+        int length = nums.length;
+        while(right<=length) {
+            if(pairCount>=k) {
+                count+= (length-right+1);
+                int curFreq = freq.getOrDefault(nums[left],0);
+                pairCount-=(curFreq-1);
+                if(curFreq==1){
+                    freq.remove(nums[left]);
+                } else {
+                    freq.put(nums[left], curFreq-1);
+                }
+                left++;
+                continue;
             }
-            j++;
+
+            if(right<nums.length) {
+                int curFreq = freq.getOrDefault(nums[right],0);
+                pairCount+=curFreq;
+                freq.put(nums[right],curFreq+1);
+            }
+            right++;
         }
-        return ans;
+
+        return count;
     }
 }
