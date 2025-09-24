c1a4a31047628f266d56da2bf6d52343caea2b8a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-09-24 06:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (51.35%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,27 +1,53 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        int length = nums.length;
         int left = 0;
         int right = nums.length-1;
 
-        while(left<=right){
+        while(left<=right) {
             int mid = (left+right)/2;
-            if(target==nums[mid])
+            if(target == nums[mid]) {
                 return true;
-
-            if(target<nums[mid]) {
-               if(target>=nums[left])
-                    right = mid-1;
-                else
-                    left++;
             }
-            else if(target>nums[mid]) {
-                 if(target<=nums[right])
+
+            if(target>nums[mid]) {
+                if(nums[mid]>nums[left]) {
                     left = mid+1;
-                else
+                } else if (nums[mid]==nums[left]) {
+                    left++;
+                } else {
+                    if(target>nums[right]) {
+                        right = mid-1;
+                    } else {
+                        left = mid+1;
+                    }
+                }
+            } else {
+                if(nums[mid]<nums[right]) {
+                    right = mid-1;
+                } else if (nums[mid]==nums[right]) {
                     right--;
+                } else {
+                    if(target>=nums[left]) {
+                        right = mid-1;
+                    } else {
+                        left = mid+1;
+                    }
+                }
             }
         }
 
         return false;
     }
 }
+
+/*
+    2, 5, 6, 0 0 1 2
+    mid => number> mid and on the right side
+           number > mid and on the left side
+            number<mid  and on  right side 
+            number<mid and on the left side
+
+    number>mid and number>rightmost => left
+    number>mid and number<rightmost => 
+*/
