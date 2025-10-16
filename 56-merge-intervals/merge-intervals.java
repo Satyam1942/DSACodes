bfd195d19a1ae179ec273f0ce7e995abea2e931e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-10-16 15:46:15 +0530

    Time: 8 ms (84.83%) | Memory: 46.8 MB (40.73%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,49 +1,27 @@
 class Solution {
-    public int[][]  merge(int[][] intervals) {
-    //    input: [[1,3],[2,6],[8,10],[15,18]]
-    //    output: [[1,6],[8,10],[15,18]]
-         
-    //     List: [1,3] 1<2<3
-    //           [1,6] 8>6 create a new interval 
-
-        Arrays.sort(intervals, (a,b)->(a[0]-b[0]));
+    public int[][] merge(int[][] intervals) {
         int length = intervals.length;
-        List<int[]> mergedIntervals = new ArrayList<>();
+        List<int[]> mergedIntervals  = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)->(a[0]-b[0]));
 
-        for(int index =0;index<length;index++)
-        {
-            int newStart = intervals[index][0];
-            int newFinish = intervals[index][1];
-
-            if(mergedIntervals.isEmpty() || 
-            newStart>mergedIntervals.get(mergedIntervals.size()-1)[1])
-            {
-                mergedIntervals.add(intervals[index]);
-            }
-            else
-            {
-                int oldStart =  mergedIntervals.get(mergedIntervals.size()-1)[0];
-                int oldFinish =  mergedIntervals.get(mergedIntervals.size()-1)[1];
-                int mergedFinish = Math.max(newFinish,oldFinish);
-                
-                int newInterval[] = new int[2];
-                newInterval[0] = oldStart;
-                newInterval[1] = mergedFinish;
-
-                mergedIntervals.set(mergedIntervals.size()-1,newInterval); 
+        mergedIntervals.add(new int[]{intervals[0][0], intervals[0][1]});
+        for(int i=1; i<length; i++) {
+            int curStart= intervals[i][0];
+            int curEnd = intervals[i][1];
+            int prevStart = mergedIntervals.get(mergedIntervals.size()-1)[0];
+            int prevEnd = mergedIntervals.get(mergedIntervals.size()-1)[1];
+            if(curStart>prevEnd) {
+                mergedIntervals.add(new int[]{curStart, curEnd});
+            } else {
+                int newEnd = Math.max(curEnd, prevEnd);
+                mergedIntervals.set(mergedIntervals.size()-1, new int[]{prevStart, newEnd});
             }
         }
 
-        
-
-        int[][] ans = new int[mergedIntervals.size()][2];
-        int pointer =0;
-        for(int[] interval:mergedIntervals)
-        {
-            ans[pointer] = interval;
-            pointer++;
+        int ans[][] = new int[mergedIntervals.size()][2];
+        for(int i = 0; i<mergedIntervals.size(); i++) {
+            ans[i] = mergedIntervals.get(i);
         }
-
         return ans;
     }
 }
