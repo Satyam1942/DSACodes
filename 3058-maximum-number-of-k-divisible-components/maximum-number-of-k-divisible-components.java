be3ceff5d693b45daef5d854fdfbf3c74642ae12
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2025-11-29 08:33:46 +0530

    Time: 32 ms (39.36%) | Memory: 92.6 MB (20.21%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -1,61 +1,44 @@
-class Pair {
-    int node;
-    int val;
-    Pair(int node, int val) {
-        this.node = node;
-        this.val = val;
-    }
-}
-
 class Solution {
-
-    int maxCuts = 0;
-
-    long dfs(int node, List<List<Pair>> adjList, int[] values, HashSet<Integer> vis, int k) {
-        long sum = values[node];
-        vis.add(node);
-
-        for(int i=0; i<adjList.get(node).size(); i++) {
-            int adjNode = adjList.get(node).get(i).node;
-            if(!vis.contains(adjNode)) {
-                long val = dfs(adjNode, adjList, values, vis, k);
-                sum+=val;
+    int compCount = 0;
+    long postOrderTraversal(int curNode, int vis[], int k, int values[], List<List<Integer>> adjList) {
+        vis[curNode] = 1;
+        long sum = 0;
+        for(int i=0; i<adjList.get(curNode).size(); i++) {
+            int adjNode = adjList.get(curNode).get(i);
+            if(vis[adjNode]==0) {
+                sum+= postOrderTraversal(adjNode, vis, k, values, adjList);
             }
         }
-
-        if(sum%k==0) {
-            maxCuts++;
-            sum = 0;
+        sum+=values[curNode];
+        if(sum%k == 0) {
+            compCount++;
+            return 0;
+        } else {
+            return sum;
         }
-
-        return sum; 
     }
-
     public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
-        List<List<Pair>> adjList = new ArrayList<>();
-        HashSet<Integer> vis = new HashSet<>();
-
+        List<List<Integer>> adjList = new ArrayList<>();
+        int vis[] = new int[n];
         for(int i=0; i<n; i++) {
             adjList.add(new ArrayList<>());
         }
-
         for(int i=0; i<n-1; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adjList.get(u).add(new Pair(v,values[v]));
-            adjList.get(v).add(new Pair(u,values[u]));
+            adjList.get(edges[i][0]).add(edges[i][1]);
+            adjList.get(edges[i][1]).add(edges[i][0]);
         }
-        
-        dfs(0,adjList,values, vis, k);
-        return maxCuts;
+        postOrderTraversal(0, vis,k, values, adjList);
+        return compCount;
     }
 }
 
 /*
+    0 1 2 3 4 
 
-        8
-            4
-    1
-  4   1
+    hashmap :
+        4
+        5
+        1
         
+
 */
