d8db364dbb0b1e4a61f9de65225859569925ba4b
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2026-03-14 09:09:59 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (87.15%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,58 +1,69 @@
 class Solution {
+    String recursion(int n, int k, char prevChar) {
+        if(n==0) {
+            return  "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int maxHappyString = (1<<(n));
+        int modulus =  k%(maxHappyString/2);
+        int region = (modulus == 0)? k/(maxHappyString/2): k/(maxHappyString/2)+1;
+        modulus = (modulus==0)? maxHappyString/2 : modulus; 
+        char curChar = '\0';
+
+        switch(prevChar) {
+            case 'a' :
+                if(region == 1) {
+                    curChar = 'b';
+                } else {
+                    curChar =  'c';
+                }
+                break;
+            case 'b' :
+                if(region == 1) {
+                    curChar = 'a';
+                } else {
+                    curChar = 'c';
+                }
+                break;
+            case 'c' :
+                if(region == 1) {
+                    curChar = 'a';
+                } else {
+                    curChar = 'b';
+                }
+                break;
+        }
+
+        String newStr = recursion(n-1, modulus, curChar);
+        sb.append(curChar);
+        sb.append(newStr);
+        return sb.toString();
+    }
+
     public String getHappyString(int n, int k) {
-        if(k>(3*(1<<(n-1)))) {
+        int maxHappyString = (1<<(n-1))*3;
+        if(k>maxHappyString) {
             return "";
         }
-    
+
         StringBuilder sb = new StringBuilder();
-        k--;
-        
-        int index  = k/(1<<(n-1));
-        char token = (char)(index+97);
-        char prev = token;
-        sb.append(token);
-        k = k%(1<<(n-1));
-        n--;
+        int modulus =  k%(maxHappyString/3);
+        int region = (modulus == 0)? k/(maxHappyString/3): k/(maxHappyString/3)+1;
+        modulus = (modulus==0)? maxHappyString/3 : modulus; 
+        char prevChar = '\0';
 
-        while(n>0) {
-            index  = k/(1<<(n-1));
-            // System.out.println(index);
-            if(index==0) {
-                if(prev=='a') {
-                    token = 'b';
-                } else {
-                    token = 'a';
-                }
-            } else {
-                if(prev=='c') {
-                    token = 'b';
-                } else {
-                    token = 'c';
-                }
-            }
-
-            prev = token;
-            sb.append(token);
-            k = k%(1<<(n-1));
-            n--;
+        if(region == 1) {
+            prevChar = 'a';
+        } else if(region == 2) {
+            prevChar = 'b';
+        } else {
+            prevChar = 'c';
         }
 
+        String nStr =  recursion(n-1, modulus, prevChar);
+        sb.append(prevChar);
+        sb.append(nStr);
         return sb.toString();
     }
 }
-
-/*
- k/2^n-1
- k = k%2^n-1 ; n--;
-
- 9/4  = 2 => c
- k = 1, n = 2;
- 1/2 =  0 => a
- k = 1 ; n = 1
- 1/1 = 1 =>b
- k = 0 ; n = 0;
- break; 
-
-
-
-*/
