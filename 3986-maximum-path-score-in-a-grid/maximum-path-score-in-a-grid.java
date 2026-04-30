679fe47660b1613e4544257e5c2f37775fef7a7e
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2026-04-30 08:47:38 +0530

    Time: 506 ms (5.38%) | Memory: 275.8 MB (5.92%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.java
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.java
@@ -1,54 +1,56 @@
 class Solution {
-    int recursion(int i, int j, int grid[][], int k, Integer cache[][][]) {
-        int length = grid.length;
-        int width = grid[0].length;
-        
-        if(i==length-1 && j==width-1) {
-            if(grid[i][j]==0) {
-                return 0;
-            } else {
-                if(k==0) {
-                    return Integer.MIN_VALUE;
-                } else {
-                    return grid[i][j];
-                }
-            }
-        }
-
-        if(cache[i][j][k] != null) {
-            return cache[i][j][k];
-        }
-
-        int right = Integer.MIN_VALUE;
-        int down = Integer.MIN_VALUE;
-        int score = grid[i][j];
-        int deltaK = (score == 0)? 0 : 1;
-
-        if(i+1<length && k-deltaK>=0) {
-            down = recursion(i+1, j, grid, k-deltaK, cache);
-        } 
-        if(j+1<width && k-deltaK>=0) {
-            right = recursion(i, j+1, grid, k-deltaK, cache);
-        }
-
-        int finalScore =  Math.max(down, right);
-        if(finalScore != Integer.MIN_VALUE) {
-            finalScore+=score;
-        }
-
-        return cache[i][j][k] = finalScore;
-    }
-
     public int maxPathScore(int[][] grid, int k) {
         int length = grid.length;
         int width = grid[0].length;
         Integer cache[][][] = new Integer[length][width][k+1];
 
-        int maxScore =  recursion(0, 0, grid, k, cache);
+        for(int i=0;i<=k;i++) {
+            if(grid[length-1][width-1]==0) {
+                cache[length-1][width-1][i] =  0;
+            } else {
+                if(i==0) {
+                    cache[length-1][width-1][i] = Integer.MIN_VALUE;
+                } else {
+                    cache[length-1][width-1][i] = grid[length-1][width-1];
+                }
+            }            
+        }
+
+        for(int i = length-1; i>=0; i--) {
+            for(int j=width-1;j>=0; j--) {
+                if(i==length-1 && j==width-1) {
+                    continue;
+                }
+                for(int l=0; l<=k; l++) {
+                    int right = Integer.MIN_VALUE;
+                    int down = Integer.MIN_VALUE;
+                    int score = grid[i][j];
+                    int deltaK = (score == 0)? 0 : 1;
+
+                    if(i+1<length && l-deltaK>=0) {
+                        down = cache[i+1][j][l-deltaK];
+                    }            
+                    if(j+1<width && l-deltaK>=0) {
+                        right = cache[i][j+1][l-deltaK];
+                    }
+
+                    int finalScore =  Math.max(down, right);
+                    if(finalScore != Integer.MIN_VALUE) {
+                        finalScore+=score;
+                    }
+                    
+                    cache[i][j][l] = finalScore;
+                }
+            }
+        }
+
+        int maxScore = cache[0][0][k];
+
         if(maxScore == Integer.MIN_VALUE) {
             return -1;
         } else {
             return maxScore;
         }
+
     }
 }
