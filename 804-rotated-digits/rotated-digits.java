d09b1d514ced78884c47b5b296539c1fa118b930
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2026-05-02 07:55:16 +0530

    Time: 1 ms (93.44%) | Memory: 42.2 MB (56.33%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -9,34 +9,55 @@ class Solution {
         rotationMap.put(9, 6);
     }
 
-    boolean isGoodNumber(int n, HashMap<Integer, Integer> rotationMap) {
-        int x  = n;
-        int newNum = 0;
-        int mult = 1;
-        while(x>0) {
-            int d = x%10;
-            if(!rotationMap.containsKey(d)) {
-                return false;
+    int countGoodNumber(int index, int restricted, int notEqual, int[] num, HashMap<Integer, Integer> rotationMap, Integer cache[][][]) {
+        if(index == num.length) {
+            if(notEqual == 1) {
+                return 1;
+            } else {
+                return 0;
             }
-            d = rotationMap.get(d);
-            newNum = mult*d + newNum;
-            mult*=10;
-            x =x/10;
         }
-        return newNum!=n;
+
+        if(cache[index][restricted][notEqual]!=null) {
+            return cache[index][restricted][notEqual];
+        }
+
+        int limit = (restricted==1)? num[index] : 9;
+        int count = 0;
+        for(Map.Entry<Integer, Integer> i: rotationMap.entrySet()) {
+            if(i.getKey()>limit) {
+                continue;
+            }
+            int isNotSameDigit = (i.getValue()==i.getKey()) ? 0 : 1;
+            int isSame = (i.getKey()==num[index])? 1:0 ;
+            int newRestricted = Math.min(restricted, isSame);
+            int newNotEqual = Math.max(notEqual, isNotSameDigit);
+            count += countGoodNumber(index+1, newRestricted, newNotEqual, num, rotationMap, cache); 
+        }
+        return cache[index][restricted][notEqual] = count;
     }
 
-    public int rotatedDigits(int n) {
-        int count = 0; 
-        HashMap<Integer, Integer> rotationMap = new HashMap<>();
-        getRotationMap(rotationMap);
-        for(int i=1; i<=n; i++) {
-            if(isGoodNumber(i, rotationMap)) {
-                count++;
-            }
+    int [] createArray(int n) {
+        int noOfDigits = (int)Math.floor(Math.log10(n))+1;
+        int arr[] = new int[noOfDigits];
+        int j = arr.length-1;
+        while(n>0) {
+            int d=n%10;
+            arr[j] = d;
+            j--;
+            n/=10;
         }
-        return count;
+        return arr;
+    }
+    public int rotatedDigits(int n) {
+        Integer cache[][][] = new Integer[10][2][2]; 
+        HashMap<Integer, Integer> rotationMap = new HashMap<>();
+        int numArr[] = createArray(n);
+        int count = 0;
+        getRotationMap(rotationMap);
+        count = countGoodNumber(0, 1, 0, numArr, rotationMap, cache);
 
+        return count;
     }
 }
 
